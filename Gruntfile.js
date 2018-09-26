2125557aa8e65044a9f7fa28349cc4ea1c0a74ca
Naomi Dushay <ndushay@stanford.edu>
2018-09-26 14:07:23 -0700

    Gruntfile: reformatted whitespace so tasks line up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,47 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                stripBanners: true,
-                banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */',
-            },
-            dist: {
-                files: {
-                'builds/bfe.js' : ['build_support/mini_require.js','src/bfe.js','src/bfestore.js','src/bfelogging.js','src/bfelookups.js','src/lib/aceconfig.js'], 
-                'builds/bfe.css' : ['src/css/bootstrap.css', 'src/css/typeahead.css'],
-                }
-            },
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        stripBanners: true,
+        banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      dist: {
+        files: {
+          'builds/bfe.js': ['build_support/mini_require.js', 'src/bfe.js', 'src/bfestore.js', 'src/bfelogging.js', 'src/bfelookups.js', 'src/lib/aceconfig.js'],
+          'builds/bfe.css': ['src/css/bootstrap.css', 'src/css/typeahead.css'],
+        }
+      },
+    },
+    uglify: {
+      options: {
+        stripBanners: true,
+        banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      dist: {
+        files: [{
+          src: 'builds/bfe.js',
+          dest: 'builds/bfe.min.js'
+        }]
+      },
+    },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
         },
-		uglify: {
-			options: {
-                stripBanners: true,
-				banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */',
-			},
-			dist: {
-				files: [
-					{src: 'builds/bfe.js', dest: 'builds/bfe.min.js'}
-				]
-			},
-		},
-        cssmin: {
-            add_banner: {
-                 options: {
-                     banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
-                 },
-            },
-            combine: {
-                 files: {
-                      'builds/bfe.min.css': ['builds/bfe.css']
-                 }
-            }
-         }
-	});
+      },
+      combine: {
+        files: {
+          'builds/bfe.min.css': ['builds/bfe.css']
+        }
+      }
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-uglify-es');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-	
-	grunt.registerTask('default', ['concat','uglify', 'cssmin']);
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 };
