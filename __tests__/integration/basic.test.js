3944d029f7f6e86a9aa6b7fce3dfd0f7cb13d2ac
Naomi Dushay <ndushay@stanford.edu>
2018-10-01 14:47:32 -0700

    initial basic integration test added

---
+++ __tests__/integration/basic.test.js
@@ -0,0 +1,9 @@
+describe('Basic end to end Sinopia BIBFRAME Editor', () => {
+  beforeAll(async () => {
+    await page.goto('http://127.0.0.1:8000/');
+  });
+
+  it('displays "Bibframe Editor Workspace" text', async () => {
+    await expect(page).toMatch('Bibframe Editor Workspace')
+  });
+});
