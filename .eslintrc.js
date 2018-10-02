25495e8682a72313d266bb69c8dd9182edf03b03
Naomi Dushay <ndushay@stanford.edu>
2018-10-02 11:34:41 -0700

    eslint hooked up to CI and fails if warnings increase (beyond 1319!!!)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,18 @@
-const ERROR = 2;
-const WARN = 1;
-
 module.exports = {
-    extends: [ "eslint:recommended", "plugin:node/recommended", "plugin:security/recommended"],
+    extends: [ "eslint:recommended", "plugin:node/recommended", "plugin:security/recommended", "plugin:jest/recommended"],
     overrides: [
       {
         files: "**/*.js",
         env: {
           jest: true
         },
-        plugins: [
-          "jest", "security"
-        ],
+        plugins: ["jest", "security"],
         rules: {
-          "jest/no-disabled-tests": "warn",
-          "jest/no-focused-tests": "error",
-          "jest/no-identical-title": "error",
-          "jest/prefer-to-have-length": "warn",
-          "jest/valid-expect": "error",
-          "node/exports-style": ["error", "module.exports"],
-          "node/prefer-global/buffer": ["error", "always"],
-          "node/prefer-global/console": ["error", "always"],
-          "node/prefer-global/process": ["error", "always"],
-          "node/prefer-global/url-search-params": ["error", "always"],
-          "node/prefer-global/url": ["error", "always"],
+          "no-console": "warn",
+          "no-redeclare": "warn", // FIXME: want this to be error, but don't want to address in 5000 line bfe.js
+          "no-undef": "warn",
+          "no-unused-vars": "warn",
+          "no-useless-escape": "warn"
         }
       }
   ]
