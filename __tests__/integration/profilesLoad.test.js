13ac4f90e5efc58a8d7e31b2a6794a9428f4c0fe
Naomi Dushay <ndushay@stanford.edu>
2018-10-15 17:30:48 -0700

    test showing monograph instance profile loads

---
+++ __tests__/integration/profilesLoad.test.js
@@ -0,0 +1,26 @@
+describe('Profiles load', () => {
+
+  beforeAll(async () => {
+    // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    await page.goto('http://127.0.0.1:8000/__tests__/integration/index.html');
+    await expect(page).toClick('a[href="#create"]', { text: 'Editor' })
+    await page.waitFor(1000)
+  });
+
+  it('loads profile for Monograph Instance', async () => {
+
+    const monograph_sel = 'ul.nav-sidebar > li:first-child > a:first-child'
+    await expect(page).toClick(monograph_sel, { text: 'Monograph' })
+
+    const instance_sel = 'a#sp-0_0'
+    await expect(page).toClick(instance_sel, { text: 'Instance' })
+
+    await page.waitFor(1000)
+
+    // await page.screenshot({path: 'screenshot.png', fullPage: true})
+
+    const ptitle_sel = 'div#bfeditor-formdiv > form > div > h4'
+    const profile_title = await page.$eval(ptitle_sel, e => e.textContent)
+    await expect(profile_title).toMatch(/BIBFRAME Instance/)
+  })
+})
