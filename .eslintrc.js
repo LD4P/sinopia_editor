471260189ddbd7318e799407be9e5aeee44110cf
Naomi Dushay <ndushay@stanford.edu>
2018-10-15 18:02:48 -0700

    greatly reduce eslint warnings by adding more globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,14 +13,23 @@ module.exports = {
       },
       plugins: ["jest", "security"],
       rules: {
-        "no-console": "warn",
+        "no-console": "off",
         "no-redeclare": "warn", // FIXME: want this to be error, but don't want to address in 5000 line bfe.js
         "no-undef": "warn",
         "no-unused-vars": "warn",
         "no-useless-escape": "warn"
       },
       globals: {
-        "page": true
+        // FIXME: this is a cheap way to reduce warnings, but perhaps code practices should improve for our own stuff?
+        "$": true,
+        "bfe": true,
+        "bfeditor": true,
+        "bfelog": true,
+        "config": true,
+        "document": true,
+        "jQuery": true,
+        "page": true,
+        "window": true
       }
     }
   ]
