cc4469f11ec8e3aa657968c92edfd2d749428552
Naomi Dushay <ndushay@stanford.edu>
2018-10-16 14:53:30 -0700

    jest-puppeteer-config: re-disable web security

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   launch: {
     headless: process.env.HEADLESS !== 'false',
+    'args' : [ '--disable-web-security' ] // in order to access verso api
   },
   server: {
     command: 'node server-bfe.js',
