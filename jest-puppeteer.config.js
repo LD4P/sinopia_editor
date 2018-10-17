07f22ae5a66399a8602fd90689da87e86f515de0
Naomi Dushay <ndushay@stanford.edu>
2018-10-17 14:02:49 -0700

    jest-puppeteer.config.js: comment why disable-web-security

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   launch: {
     headless: process.env.HEADLESS !== 'false',
-    'args' : [ '--disable-web-security' ],
+    'args' : [ '--disable-web-security' ], // avoid CORS blocking verso requests
   },
   server: {
     command: 'node server-bfe.js',
