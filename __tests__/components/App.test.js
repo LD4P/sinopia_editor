06159c4779c0173626c0448ab014581e40e026f9
Sarav Shah <saravsshah@gmail.com>
2018-10-29 16:21:16 -0700

    Setup Enzyme, mock out importing of PNGs, introduce dependency for css module imports, add App.js and Header.js tests

--- __tests__/components/App.test.js
+++ __tests__/components/App.test.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../src/components/App';
+import Header from '../../src/components/Header';
+import NewsPanel from '../../src/components/NewsPanel';
+
 
 describe('<App />', () =>{
-  it("contains a div with an id", () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper.contains(<div id="app" />)).to.equal(true);
-  })
-})
+  it('should be selectable by id "app"', () => {
+    expect(shallow(<App />).is('#app')).toBe(true);
+  });
+
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <App>
+        <Header />
+        <NewsPanel />
+      </App>
+    ));
+    expect(wrapper.contains(<Header />)).toBe(true);
+    expect(wrapper.contains(<NewsPanel />)).toBe(true);
+  });
+
+  it('renders an "#app"', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('#app').length).toBe(1);
+  });
+
+  it('renders two <App /> components', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header).length).toBe(1);
+    expect(wrapper.find(NewsPanel).length).toBe(1);
+  });
+
+
+});
---
+++ __tests__/components/Header.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow, render } from 'enzyme';
+import Header from '../../src/components/Header';
+import SinopiaLogo from '../../styles/sinopia-logo.png'
+
+describe('<Header />', () => {
+  it('renders the Sinopia image', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find("img").prop('src')).toEqual(SinopiaLogo);
+  });
+
+  it ('renders a ".navbar" w/ 4 dropdown menu options', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('.navbar').length).toBe(1);
+    expect(wrapper.find('.divider').length).toBe(4);
+  });
+
+  it ('renders a dropdown menu option', () => {
+    const wrapper = shallow(<Header />)
+    expect(wrapper.find('a[href="#"]').at(0).text()).toEqual("Help and Resources")
+    expect(wrapper.find('a[href="#"]').at(3).text()).toEqual("Website Usage")
+  });
+});
+
