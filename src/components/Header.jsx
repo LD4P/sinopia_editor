c6e78ca20a7bee09b32697e04ee67d62cce7ef62
Sarav Shah <saravsshah@gmail.com>
2018-10-30 16:38:58 -0700

    Add more menu drop down options and some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,20 +10,41 @@ const Header = () => (
     </div>
     <ul className= "nav navbar-nav pull-right">
       <li>
-        <a href="https://google.com">Profile Editor</a>
+        <a className="header-text" href="https://google.com">Profile Editor</a>
       </li>
       <li className="dropdown">
-        <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Help and Resources<span className="caret"></span></a>
+        <a href="#" className="dropdown-toggle header-text" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Help and Resources<span className="caret"></span></a>
         <ul className="dropdown-menu">
-          <li><a href="#">Contact Us</a></li>
+          <li><a href="#" ><strong>Sinopia User Dashboard</strong></a></li>
           <li role="separator" className="divider"></li>
-          <li><a href="#">Training Resources</a></li>
+          <li><a href="#"><strong>Contact Us</strong></a></li>
+          <li><a href="#" >E-mail Sinopia group</a></li>
+          <li><a href="https://ld4.slack.com/messages/#sinopia" >Join Slack Channel</a></li>
           <li role="separator" className="divider"></li>
-          <li><a href="#">Website Usage</a></li>
+          <li><a href="#"><strong>Training Resources</strong></a></li>
+          <li><a href="#" >Intro to Profile Editor</a></li>
+          <li><a href="#" >Intro to Bibliographic Editor</a></li>
+          <li><a href="#" >Troubleshooting</a></li>
           <li role="separator" className="divider"></li>
-          <li><a href="#">External Resources</a></li>
+          <li><a href="#"><strong>Website Usage</strong></a></li>
+          <li><a href="#" >Policies</a></li>
+          <li><a href="#" >Terms of Use</a></li>
+          <li><a href="#" >Request Account</a></li>
           <li role="separator" className="divider"></li>
-          <li><a href="#">One more separated link</a></li>
+          <li><a href="#"><strong>External Resources</strong></a></li>
+          <li><a href="http://www.getty.edu/research/tools/vocabularies/aat/" >Art & Architecture Thesaurus</a></li>
+          <li><a href="https://www.discogs.com/search/" >Discogs</a></li>
+          <li><a href="http://www.geonames.org/" >Geonames</a></li>
+          <li><a href="https://translate.google.com/" >Google Translate</a></li>
+          <li><a href="https://www.imdb.com/" >IMDB</a></li>
+          <li><a href="http://www.isni.org/" >ISNI</a></li>
+          <li><a href="http://id.loc.gov/authorities/names.html" >Library of Congress</a></li>
+          <li><a href="https://musicbrainz.org/" >MusicBrainz</a></li>
+          <li><a href="http://www.share-vde.org/sharevde/clusters?l=en" >Share-VDE</a></li>
+          <li><a href="http://www.loc.gov/pictures/collection/tgm/" >Thesaurus of Graphic Materials</a></li>
+          <li><a href="http://www.getty.edu/research/tools/vocabularies/tgn/index.html" >Thesaurus of Geographical Names</a></li>
+          <li><a href="https://viaf.org/" >VIAF</a></li>
+          <li><a href="https://www.wikidata.org/wiki/Wikidata:Main_Page" >Wikidata</a></li>
         </ul>
       </li>
     </ul>
--- src/components/NewsPanel.jsx
+++ src/components/NewsPanel.jsx
@@ -9,9 +9,9 @@ const NewsPanel = () => (
           <div className="col-md-7">
             <h1> Latest News </h1>
             <ul>
-            <li>We are looking for a Wikipedian in residence to join our team and boost our productivity. Send applications to hiring at dlss dot com.</li>
-            <p></p>
-            <li>We hit our January milestone and pushed the latest code to production. Try these new features and provide feedback to the Google group.</li>
+              <li>We are looking for a Wikipedian in residence to join our team and boost our productivity. Send applications to hiring at dlss dot com.</li>
+              <p></p>
+              <li>We hit our January milestone and pushed the latest code to production. Try these new features and provide feedback to the Google group.</li>
             </ul>
           </div>
           <div className="col-md-4 login-sec">
