117677c7546b6f553a88eceec02e4042a0db4a78
Sarav Shah <saravsshah@gmail.com>
2018-10-30 11:38:36 -0700

    Add Description Panel under News Panel

---
+++ src/components/DescPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DescPanel = () => (
+  <div className="desc-panel">
+    <h4>The underdrawing for the new world of linked data in libraries</h4>
+    <p>With the support of a $1.5 million dollar grant from the Andrew W. Mellon Foundation, Linked Data for Libraries: LD4L Labs is a collaboration of Cornell, Harvard, Iowa, and Stanford to continue to advance the use and usefulness of linked data in libraries. Project team members will create and assemble tools, ontologies, services, and approaches that use linked data to improve the discovery, use, and understanding of scholarly information resources. The goal is to pilot tools and services and to create solutions that can be implemented in production at research libraries within the next three to five years.</p>
+    <p>Sinopia is one of those tools which will allow the team to link metadata. Just as the term, Sinopia, means the underdrawing to prepare a fresco for painting, Sinopia, the LD4P project, will be the underdrawing preparing the team to launch a metadata editor for linked data.</p>
+  </div>
+);
+
+export default DescPanel;
