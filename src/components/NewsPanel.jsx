113ba663c16aa836a53582470b5ecd9614817ddd
Sarav Shah <saravsshah@gmail.com>
2018-10-30 10:01:04 -0700

    In react use className not class

--- src/components/NewsPanel.jsx
+++ src/components/NewsPanel.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import LoginPanel from './LoginPanel.jsx'
 
 const NewsPanel = () => (
-  <div class="jumbotron banner center-block">
-    <div class="panel panel-default">
-      <div class="panel-body">
-        <div class="row">
-          <div class="col-md-8">
+  <div className="jumbotron banner center-block">
+    <div className="panel panel-default">
+      <div className="panel-body">
+        <div className="row">
+          <div className="col-md-8">
             <h1> Latest News </h1>
             <ul>
             <li>We are looking for a Wikipedian in residence to join our team and boost our productivity. Send applications to hiring at dlss dot com.</li>
@@ -14,7 +14,7 @@ const NewsPanel = () => (
             <li>We hit our January milestone and pushed the latest code to production. Try these new features and provide feedback to the Google group.</li>
             </ul>
           </div>
-          <div class="col-md-4 login-sec">
+          <div className="col-md-4 login-sec">
             <LoginPanel />
           </div>
         </div>
