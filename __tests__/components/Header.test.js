e520456a3d24f1ade6f8a3c1c2fd7d2dd01d4c0f
Sarav Shah <saravsshah@gmail.com>
2018-11-01 16:03:21 -0700

    Update dropdown menu jest tests

--- __tests__/components/Header.test.js
+++ __tests__/components/Header.test.js
@@ -4,21 +4,26 @@ import Header from '../../src/components/Header';
 import SinopiaLogo from '../../styles/sinopia-logo.png'
 
 describe('<Header />', () => {
-  it('renders the Sinopia image', () => {
-    const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header />);
+  it ('renders the Sinopia image', () => {
     expect(wrapper.find("img").prop('src')).toEqual(SinopiaLogo);
   });
 
   it ('renders a ".navbar" w/ 4 dropdown menu options', () => {
-    const wrapper = shallow(<Header />);
     expect(wrapper.find('.navbar').length).toBe(1);
     expect(wrapper.find('.divider').length).toBe(4);
   });
 
-  it ('renders a dropdown menu option', () => {
-    const wrapper = shallow(<Header />)
-    expect(wrapper.find('a[href="#"]').at(0).text()).toEqual("Help and Resources")
-    expect(wrapper.find('a[href="#"]').at(3).text()).toEqual("E-mail Sinopia group")
+  it ('renders a dropdown menu sections', () => {
+    expect(wrapper.find('strong').at(0).text()).toEqual("Sinopia User Dashboard")
+    expect(wrapper.find('strong').at(1).text()).toEqual("Contact Us")
+    expect(wrapper.find('strong').at(2).text()).toEqual("Training Resources")
+    expect(wrapper.find('strong').at(3).text()).toEqual("Website Usage")
+    expect(wrapper.find('strong').at(4).text()).toEqual("External Resources")
+  });
+
+  it ('renders dropdown menu links', () => {
+    expect(wrapper.find('a[href="https://ld4.slack.com/messages/#sinopia"]')).toBeDefined()
   });
 });
 
