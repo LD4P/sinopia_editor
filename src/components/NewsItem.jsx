aae0880bb10fd900ed4e0131bc074c5948079dd2
Sarav Shah <saravsshah@gmail.com>
2018-11-05 11:15:09 -0800

    Update Latest News on Home Page

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,9 +4,9 @@ const NewsItem = () => (
   <div className="news-item">
     <h1> Latest News </h1>
     <ul>
-      <li>We are looking for a Wikipedian in residence to join our team and boost our productivity. Send applications to hiring at dlss dot com.</li>
+      <li>We are looking for a Wikipedian in residence to join our team and help identify how libraries can contribute and relate their information with that of the open Web. <a target="_blank" href="http://m.rfer.us/STANFORDlAy3UB">Learn more</a></li>
       <p></p>
-      <li>We hit our January milestone and pushed the latest code to production. Try these new features and provide feedback to the Google group.</li>
+      <li>We achieved our first milestone on November 1. The Profile Editor has been released into production, and we are actively testing it.</li>
     </ul>
   </div>
 );
