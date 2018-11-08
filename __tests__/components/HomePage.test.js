93173545b5e41286c08d51e10fdfc625aa881ed7
Sarav Shah <saravsshah@gmail.com>
2018-11-08 10:32:03 -0800

    Add offset canvas to homepage w/ tests

--- __tests__/components/HomePage.test.js
+++ __tests__/components/HomePage.test.js
@@ -4,6 +4,7 @@ import HomePage from '../../src/components/HomePage'
 import Header from '../../src/components/Header'
 import NewsPanel from '../../src/components/NewsPanel'
 import DescPanel from '../../src/components/DescPanel'
+import Footer from '../../src/components/Footer'
 
 
 describe('<HomePage />', () =>{
@@ -11,25 +12,12 @@ describe('<HomePage />', () =>{
     expect(shallow(<HomePage />).is('#home-page')).toBe(true)
   })
 
-  it('renders children when passed in', () => {
-    const wrapper = shallow((
-      <HomePage>
-        <Header />
-        <NewsPanel />
-        <DescPanel />
-      </HomePage>
-    ))
-    expect(wrapper.contains(<Header />)).toBe(true)
-    expect(wrapper.contains(<NewsPanel />)).toBe(true)
-  })
-
-
-  it('renders three <HomePage /> components', () => {
+  it('renders four <HomePage /> components', () => {
     const wrapper = shallow(<HomePage />)
     expect(wrapper.find(Header).length).toBe(1)
     expect(wrapper.find(NewsPanel).length).toBe(1)
     expect(wrapper.find(DescPanel).length).toBe(1)
-
+    expect(wrapper.find(Footer).length).toBe(1)
   })
 })
 
