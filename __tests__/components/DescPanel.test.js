1f1c35594fe1265e9b696612236405ad9e3749e4
Naomi Dushay <ndushay@stanford.edu>
2018-11-08 11:21:33 -0800

    DescPanel.test: change test description

--- __tests__/components/DescPanel.test.js
+++ __tests__/components/DescPanel.test.js
@@ -3,7 +3,7 @@ import { shallow, render } from 'enzyme'
 import DescPanel from '../../src/components/DescPanel'
 
 describe('<DescPanel />', () => {
-  it ('renders the grant description above the footer', () => {
+  it ('renders the grant description', () => {
     const wrapper = shallow(<DescPanel />)
     expect(wrapper.find("div.desc-panel")).toBeDefined()
   })
