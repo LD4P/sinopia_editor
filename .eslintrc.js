45168e882623d511c93cc01911506a187d329909
Naomi Dushay <ndushay@stanford.edu>
2018-11-15 19:03:44 -0800

    eslint: use ecmaVersion 2019

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     "plugin:security/recommended"
   ],
   "parserOptions": {
-    "ecmaVersion": 2018,
+    "ecmaVersion": 2019,
     "sourceType": "module",
     "ecmaFeatures": {
         "jsx": true
