ab529e2121399c2c12e93d46378b23f07c5d20c0
Joshua Greben <jgreben@stanford.edu>
2018-11-30 15:58:32 -0800

    Fixed sinopiaServerSpoof test after rebase from master

--- __tests__/sinopiaServerSpoof.test.js
+++ __tests__/sinopiaServerSpoof.test.js
@@ -2,8 +2,8 @@ describe('sinopiaServerSpoof', () => {
   let sinopiaServerSpoof  = require('../src/sinopiaServerSpoof.js')
 
   describe('resourceTemplateIds', () => {
-    it('array of length 10', () => {
-      expect(sinopiaServerSpoof.resourceTemplateIds).toHaveLength(10)
+    it('array of length 13', () => {
+      expect(sinopiaServerSpoof.resourceTemplateIds).toHaveLength(11)
     })
     it('resourceTemplateId is in expected format', () => {
       sinopiaServerSpoof.resourceTemplateIds.forEach(id => {
@@ -13,16 +13,16 @@ describe('sinopiaServerSpoof', () => {
   })
 
   describe('resourceTemplateId2Json', () => {
-    it('array of length 10', () => {
-      expect(sinopiaServerSpoof.resourceTemplateId2Json).toHaveLength(10)
+    it('array of length 13', () => {
+      expect(sinopiaServerSpoof.resourceTemplateId2Json).toHaveLength(11)
     })
     it('mapping has id', () => {
       expect(sinopiaServerSpoof.resourceTemplateId2Json[0]['id']).toBe('resourceTemplate:bf2:Monograph:Instance')
-      expect(sinopiaServerSpoof.resourceTemplateId2Json[9]['id']).toBe('resourceTemplate:bf2:WorkVariantTitle')
+      expect(sinopiaServerSpoof.resourceTemplateId2Json[10]['id']).toBe('resourceTemplate:bf2:WorkVariantTitle')
     })
     it('mapping has json', () => {
       expect(sinopiaServerSpoof.resourceTemplateId2Json[0]['json']).toBeDefined()
-      expect(sinopiaServerSpoof.resourceTemplateId2Json[9]['json']).toBeDefined()
+      expect(sinopiaServerSpoof.resourceTemplateId2Json[10]['json']).toBeDefined()
     })
   })
 
