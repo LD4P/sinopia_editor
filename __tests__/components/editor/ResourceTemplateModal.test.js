1cc7949048e077976dd37d0bb670746adc8a51d2
Naomi Dushay <ndushay@stanford.edu>
2018-11-29 16:37:16 -0800

    ResourceTemplateModal component added

---
+++ __tests__/components/editor/ResourceTemplateModal.test.js
@@ -0,0 +1,53 @@
+// Copyright 2018 Stanford University see Apache2.txt for license
+
+import React from 'react'
+import Modal from 'react-bootstrap/lib/Modal'
+import Button from 'react-bootstrap/lib/Button'
+import { shallow } from 'enzyme'
+import ResourceTemplateForm from '../../../src/components/editor/ResourceTemplateForm'
+import ResourceTemplateModal from '../../../src/components/editor/ResourceTemplateModal'
+
+describe('<ResourceTemplateModal />', () => {
+  const propTemplates = [{key: 'value'}]
+  const tvFunc = () => {}
+  const wrapper = shallow(<ResourceTemplateModal
+                            rtId='a'
+                            modalId='a'
+                            visible={true}
+                            toggleVisibility={tvFunc}
+                            propertyTemplates={propTemplates}
+                          />)
+
+  describe('header', () => {
+    it('has a Modal.Header', () => {
+      expect(wrapper.find(Modal.Header).length).toBe(1)
+    })
+    it('has a Modal.Title inside the Modal.Header', () => {
+      expect(wrapper.find(Modal.Header).find(Modal.Title).length).toBe(1)
+    })
+  })
+
+  describe('body', () => {
+    it('has a Modal.Body', () => {
+      expect(wrapper.find(Modal.Body).length).toBe(1)
+    })
+    describe('ResourceTemplateForm', () => {
+      it('renders <ResourceTemplateForm /> component', () => {
+        expect(wrapper.find(ResourceTemplateForm).length).toBe(1)
+      })
+      it('passes props.propertyTemplates to <ResourceTemplateForm />', () => {
+        const rtForm = wrapper.find(ResourceTemplateForm)
+        expect(rtForm.props().propertyTemplates).toEqual(propTemplates)
+      })
+    })
+  })
+
+  describe('footer', () => {
+    it('has a Modal.Footer', () => {
+      expect(wrapper.find(Modal.Footer).length).toBe(1)
+    })
+    it('has two Buttons (Cancel and Save)', () => {
+      expect(wrapper.find(Modal.Footer).find(Button).length).toBe(2)
+    })
+  })
+})
