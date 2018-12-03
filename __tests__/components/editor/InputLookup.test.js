a0b1b49b7b781a8585fa8d55bd5e9781960888fb
Joshua Greben <jgreben@stanford.edu>
2018-12-03 09:46:09 -0800

    Adds test for onchange event

--- __tests__/components/editor/InputLookup.test.js
+++ __tests__/components/editor/InputLookup.test.js
@@ -43,4 +43,12 @@ describe('<InputLookup />', () => {
   it('sets the typeahead component placeholder attribute to be the propertyLabel', () => {
     expect(wrapper.find('#lookupComponent').props().placeholder).toBe('Name Lookup')
   })
+
+  it('should call onChange prop', () => {
+    const event = (wrap) => {
+      wrap.setState({options: ["{uri: 'URI', label: 'LABEL'}"]})
+    }
+    wrapper.find('#lookupComponent').simulate('change', event(wrapper))
+    expect(wrapper.state().options[0]).toBe("{uri: 'URI', label: 'LABEL'}")
+  });
 })
