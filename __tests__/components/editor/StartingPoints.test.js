153c58bc827a3c14393a42ffc67cf30f7544d1e2
Joshua Greben <jgreben@stanford.edu>
2018-12-04 14:11:49 -0800

    Adds file drop component to starting points menu using react-dropzone dependency

--- __tests__/components/editor/StartingPoints.test.js
+++ __tests__/components/editor/StartingPoints.test.js
@@ -1,14 +1,73 @@
 // Copyright 2018 Stanford University see Apache2.txt for license
 
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import StartingPoints from '../../../src/components/editor/StartingPoints'
+import DropZone from '../../../src/components/editor/StartingPoints'
+
+const jsdom = require("jsdom");
+
+setUpDomEnvironment();
 
 describe('<StartingPoints />', () => {
-  const wrapper = shallow(<StartingPoints />)
+  let wrapper = shallow(<StartingPoints />)
 
   it('Has a div with headings', () => {
     expect(wrapper.find('div > h3').text()).toEqual('Create Resource')
-    expect(wrapper.find('div > h4').text()).toEqual('Starting points:')
   })
 })
+
+describe('<DropZone />', () => {
+  let wrapper = mount(<DropZone />)
+
+  it('shows the dropzone div when button is clicked', () => {
+    wrapper.find('button.btn').simulate('click')
+    expect(wrapper.find('DropZone > section > p').text()).toEqual('Drop profile files here or click to select a file:')
+  })
+
+  it('hides the dropzone div when button is clicked again', () => {
+    wrapper.find('button.btn').simulate('click')
+    expect(wrapper.find('DropZone > section > p').exists()).toBeFalsy()
+  })
+
+  it('hides the dropzone div when the file dialog is canceled', () => {
+    wrapper.find('button.btn').simulate('click')
+    expect(wrapper.state('showDropZone')).toBeTruthy()
+    wrapper.instance().updateShowDropZone(false)
+    expect(wrapper.state('showDropZone')).toBeFalsy()
+  })
+
+  it('sets the state to include the selected file', () => {
+    wrapper.find('button.btn').simulate('click')
+    wrapper.find('input[type="file"]').simulate('drop')
+    expect(wrapper.state('files')).toBeDefined()
+  })
+
+  describe('cleanup', () => {
+    it('unmounts the wrapper', () => {
+      expect(wrapper.debug().length).toBeGreaterThanOrEqual(1)
+      wrapper.unmount();
+      expect(wrapper.debug().length).toBe(0)
+    })
+  })
+})
+
+
+function setUpDomEnvironment() {
+  const { JSDOM } = jsdom;
+  const dom = new JSDOM('<!doctype html><html><body></body></html>', {url: 'http://localhost/'});
+  const { window } = dom;
+
+  global.window = window;
+  global.document = window.document;
+  global.navigator = {
+    userAgent: 'node.js',
+  };
+  copyProps(window, global);
+}
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
--- src/components/editor/StartingPoints.jsx
+++ src/components/editor/StartingPoints.jsx
@@ -1,8 +1,35 @@
 // Copyright 2018 Stanford University see Apache2.txt for license
 
 import React, { Component }  from 'react'
+import Dropzone from 'react-dropzone'
+import PropTypes from 'prop-types'
 
 class StartingPoints extends Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+    this.onDrop = this.onDrop.bind(this)
+    this.state = {
+      files: [],
+      showDropZone: false
+    }
+  }
+
+  handleClick() {
+    let val = this.state.showDropZone
+    this.setState({ showDropZone: !val })
+  }
+
+  onDrop(files) {
+    this.setState({
+      files
+    })
+  }
+
+  updateShowDropZone = (val) => {
+    this.setState({ showDropZone: val })
+  }
+
   render() {
     let startingPoints = {
       border: '1px dotted',
@@ -10,16 +37,44 @@ class StartingPoints extends Component {
       padding: '20px'
     }
     return (
-      <div className="StartingPoints" style={startingPoints}>
-        <h3>Create Resource</h3>
-        <h4>Starting points:</h4>
-        <ul>
-          <li>Resource template 1</li>
-          <li>Resource template 2</li>
-        </ul>
-      </div>
+      <section>
+        <div className="StartingPoints" style={startingPoints}>
+          <h3>Create Resource</h3>
+          <button className="btn btn-primary btn-small" onClick={this.handleClick} >Import Profile</button>
+          { this.state.showDropZone ? <DropZone showDropZoneCB={this.updateShowDropZone} onDropCB={this.onDrop} filesCB={this.state.files}/> : null }
+        </div>
+      </section>
     )
   }
+}
+
+class DropZone extends Component {
+  render() {
+    return (
+      <section>
+        <p>Drop profile files here or click to select a file:</p>
+        <div>
+          <Dropzone
+            onFileDialogCancel={() => this.props.showDropZoneCB(false)}
+            onDrop={this.props.onDropCB.bind(this)}
+          >
+          </Dropzone>
+          <aside>
+            <h4>Dropped files:</h4>
+            <ul>
+              { this.props.filesCB.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>) }
+            </ul>
+          </aside>
+        </div>
+      </section>
+    )
+  }
+}
+
+DropZone.propTypes = {
+  onDropCB: PropTypes.func,
+  showDropZoneCB: PropTypes.func,
+  filesCB: PropTypes.array
 }
 
 export default StartingPoints;
