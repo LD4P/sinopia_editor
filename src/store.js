71d053a2b91ed1e8749b370cfd518b96dbf74b6a
Sarav Shah <saravsshah@gmail.com>
2019-01-07 14:25:10 -0800

    Comment out REDUX DEVTOOL - need to access redux store in Componenent, which causes test to fail

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux'
 import reducer from './reducers/index'
 
-const store = createStore(reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(reducer)
+  // add the following to createStore if want to use Redux plug in
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
