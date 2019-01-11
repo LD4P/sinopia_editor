1b390bcb742eaf7e1888a7e088fb5cfe1a7a70f7
Naomi Dushay <ndushay@stanford.edu>
2019-01-10 16:02:46 -0800

    schema validation of resourceTemplates upon loading in starting points  - writes valid or error messages to console; no UI flow

--- src/components/editor/StartingPoints.jsx
+++ src/components/editor/StartingPoints.jsx
@@ -3,6 +3,8 @@
 import React, { Component }  from 'react'
 import Dropzone from 'react-dropzone'
 import PropTypes from 'prop-types'
+const Ajv = require('ajv') // JSON schema validation
+const util = require('util') // for JSON schema validation errors
 
 class StartingPoints extends Component {
   constructor() {
@@ -15,16 +17,35 @@ class StartingPoints extends Component {
     }
   }
 
+  componentDidMount() {
+    this.fetchSchemaObjectsPromise()
+      .then(schemaObjs => {
+        this.ajv = new Ajv({
+          schemas: schemaObjs,
+          allErrors: true,
+          verbose: true
+        })
+      })
+      .catch(err => {console.error(`componentDidMount: error getting json schemas ${err}`)})
+  }
+
   handleClick() {
     let val = this.state.showDropZone
     this.setState({ showDropZone: !val })
   }
 
   onDropFile(files) {
-    //supplies the json loaded from the profile file
+    // supplies the json loaded from the resource template
     const handleFileRead = () => {
       const content = fileReader.result
-      this.props.setResourceTemplateCallback(content)
+      const valid = this.validateTemplate(JSON.parse(content))
+      if (valid) {
+        this.props.setResourceTemplateCallback(content)
+      }
+      else {
+        // TODO: don't continue on with display of ResourceTemplate component (#295)
+        console.error("ERROR: unable to use template as it isn't valid - we shouldn't display form")
+      }
     }
 
     let fileReader = new window.FileReader()
@@ -39,6 +60,59 @@ class StartingPoints extends Component {
 
   updateShowDropZone = (val) => {
     this.setState({ showDropZone: val })
+  }
+
+  validateTemplate = (template) => {
+    // TODO: get schema $id from template being validated (#294)
+    const valid = this.ajv.validate('https://ld4p.github.io/sinopia/schemas/0.0.1/profile.json', template)
+    // TODO:  indicate template validity to user (#295)
+    console.debug(`resource template is valid? ${valid}`)
+    if (!valid) {
+      // TODO:  indicate template errors to user (#295)
+      console.error(`template isn't valid: ${util.inspect(this.ajv.errors)}`)
+    }
+    return valid
+  }
+
+  // return array of objects - one for each fetched json schema file
+  // TODO: cache the schemas in local storage (#292)
+  fetchSchemaObjectsPromise = () => {
+    // TODO: get schema url from template json ... once it is in the template json (#294)
+    // TODO: recurse to fetch schemas from any $ref urls found in schema?
+    const schemaUrls = [
+      'https://ld4p.github.io/sinopia/schemas/0.0.1/profile.json',
+      'https://ld4p.github.io/sinopia/schemas/0.0.1/resource-templates-array.json',
+      'https://ld4p.github.io/sinopia/schemas/0.0.1/resource-template.json',
+      'https://ld4p.github.io/sinopia/schemas/0.0.1/property-templates-array.json',
+      'https://ld4p.github.io/sinopia/schemas/0.0.1/property-template.json'
+    ]
+    const schemaFetchPromises = []
+    schemaUrls.forEach( (url) => {
+      schemaFetchPromises.push(this.fetchJsonPromise(url))
+    })
+
+    return Promise.all(schemaFetchPromises)
+  }
+
+  fetchJsonPromise = (uri) => {
+    return new Promise((resolve, reject) => {
+      fetch(uri)
+      .then(resp => {
+        if (resp.ok) {
+          resp.json()
+            .then(data => {
+              resolve(data)
+            })
+            .catch(err => { reject(new Error(`Error parsing json schema ${uri} - ${err}`))})
+        }
+        else {
+          reject(new Error(`HTTP error fetching schema: ${resp.status} - ${resp.statusText}`))
+        }
+      })
+      .catch((err) => {
+        reject(new Error(`Error fetching schema ${uri} - ${err}`))
+      })
+    })
   }
 
   render() {
