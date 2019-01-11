a35f29ca3fcaaa5da4015c0c65751b5378643956
Sarav Shah <saravsshah@gmail.com>
2019-01-11 09:33:20 -0800

    update store.js comment to add debugging

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import { createStore } from 'redux'
 import reducer from './reducers/index'
 
 const store = createStore(reducer)
-  // add the following to createStore if want to use Redux plug in
+  // add the following to createStore if want to use Redux plug in 
+  // for debugging
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
