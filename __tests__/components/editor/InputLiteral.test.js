42e6735e480a09d747d1202230cb644f81dd82e7
Naomi Dushay <ndushay@stanford.edu>
2019-02-12 12:10:00 -0800

    InputLiteral unit test: move tests inside outer block

--- __tests__/components/editor/InputLiteral.test.js
+++ __tests__/components/editor/InputLiteral.test.js
@@ -194,11 +194,10 @@ describe('when there is a default literal value in the property template', () =>
       }
     )
   })
-})
 
-describe('<InputLiteral /> when repeatable="false"', () => {
-  const nrProps = {
-    "propertyTemplate":
+  describe('when repeatable="false"', () => {
+    const nrProps = {
+      "propertyTemplate":
       {
         "propertyLabel": "Instance of",
         "propertyURI": "http://id.loc.gov/ontologies/bibframe/instanceOf",
@@ -206,27 +205,28 @@ describe('<InputLiteral /> when repeatable="false"', () => {
         "mandatory": "",
         "repeatable": "false"
       }
-  }
+    }
 
-  const mockMyItemsChange = jest.fn()
-  const mockRemoveItem = jest.fn()
+    const mockMyItemsChange = jest.fn()
+    const mockRemoveItem = jest.fn()
 
-  const nonrepeat_wrapper = shallow(
-    <InputLiteral {...nrProps}
-      id={11}
-      rtId={'resourceTemplate:bf2:Monograph:Instance'}
-      handleMyItemsChange={mockMyItemsChange}
-      handleRemoveItem={mockRemoveItem} />)
+    const nonrepeat_wrapper = shallow(
+      <InputLiteral {...nrProps}
+        id={11}
+        rtId={'resourceTemplate:bf2:Monograph:Instance'}
+        handleMyItemsChange={mockMyItemsChange}
+        handleRemoveItem={mockRemoveItem} />)
 
-  it('input has disabled attribute set to "true" when repeatable is "false" and an item is added', () => {
-    nonrepeat_wrapper.find('input').simulate("change", { target: { value: "fooby" }})
-    nonrepeat_wrapper.find('input').simulate('keypress', {key: 'Enter', preventDefault: () => {}})
-    nonrepeat_wrapper.setProps({formData: { id: "http://id.loc.gov/ontologies/bibframe/instanceOf", items: [{content: "fooby", id: 0}]} })
-    expect(nonrepeat_wrapper.find('input').props('disabled')).toBeTruthy()
-  })
+    it('input has disabled attribute set to "true" when repeatable is "false" and an item is added', () => {
+      nonrepeat_wrapper.find('input').simulate("change", { target: { value: "fooby" }})
+      nonrepeat_wrapper.find('input').simulate('keypress', {key: 'Enter', preventDefault: () => {}})
+      nonrepeat_wrapper.setProps({formData: { id: "http://id.loc.gov/ontologies/bibframe/instanceOf", items: [{content: "fooby", id: 0}]} })
+      expect(nonrepeat_wrapper.find('input').props('disabled')).toBeTruthy()
+    })
 
-  it('input no longer disabled if item is removed when repeatable="false"', () => {
-    nonrepeat_wrapper.find('button#displayedItem').first().simulate('click', { target: { "dataset": {"item": 0 }}})
-    expect(nonrepeat_wrapper.find('input').props().disabled).toBeFalsy()
+    it('input no longer disabled if item is removed when repeatable="false"', () => {
+      nonrepeat_wrapper.find('button#displayedItem').first().simulate('click', { target: { "dataset": {"item": 0 }}})
+      expect(nonrepeat_wrapper.find('input').props().disabled).toBeFalsy()
+    })
   })
 })
