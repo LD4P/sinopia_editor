c48debb14b08a19f1a30e6dc54eef2064596ddb7
Joshua Greben <jgreben@stanford.edu>
2019-03-12 11:34:08 -0700

    Adds test for localStorage and expiry time in authenticate reducer

--- __tests__/reducers/authenticate.test.js
+++ __tests__/reducers/authenticate.test.js
@@ -9,14 +9,24 @@ describe('changing the reducer state', () => {
   })
 
   it('handles LOG_IN', () => {
+    let time = new Date()
+    const expiry = time.setSeconds(time.getSeconds() + 3600)
+
     expect(
       authenticate({loginJwt: {}}, {
         type: 'LOG_IN',
         payload: {id_token: '1a2b3c', access_token: 'a1b2c3', expires_in: 3600, username: 'some-user', isAuthenticated: true}
       })
-    ).toEqual(
+    ).toMatchObject(
       {loginJwt: {id_token: '1a2b3c', access_token: 'a1b2c3', username: 'some-user', isAuthenticated: true}}
     )
+
+    const expires = authenticate({loginJwt: {}}, {
+      type: 'LOG_IN',
+      payload: {id_token: '1a2b3c', access_token: 'a1b2c3', expires_in: 3600, username: 'some-user', isAuthenticated: true}
+    }).loginJwt['expiry']
+
+    expect(expires).toBeGreaterThanOrEqual(expiry)
   })
 
   it('handles LOG_OUT', () => {
@@ -25,7 +35,7 @@ describe('changing the reducer state', () => {
         type: 'LOG_OUT'
       })
     ).toEqual(
-      {loginJwt: {id_token: '', access_token: '', username: '', isAuthenticated: false}}
+      {loginJwt: {id_token: '', access_token: '', username: '', isAuthenticated: false, expiry: 0}}
     )
   })
 
