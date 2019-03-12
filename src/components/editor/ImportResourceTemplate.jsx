623c5eacac50505e21c6fae62e0f15a829657c31
Joshua Greben <jgreben@stanford.edu>
2019-03-12 09:05:55 -0700

    Redirect to editor with resource template data as state after Startingpoints callback gets data

--- src/components/editor/ImportResourceTemplate.jsx
+++ src/components/editor/ImportResourceTemplate.jsx
@@ -1,7 +1,7 @@
 // Copyright 2018 Stanford University see Apache2.txt for license
 
 import React, {Component} from 'react'
-const { getResourceTemplate } = require('../../sinopiaServerSpoof.js')
+import { Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Header from './Header'
 import StartingPoints from './StartingPoints'
@@ -9,54 +9,33 @@ import StartingPoints from './StartingPoints'
 class ImportResourceTemplate extends Component {
   constructor(props) {
     super(props)
-
-    // TODO: temporarily hardcoded here.
-    //  Selecting a resource template will happen in the left-nav "Starting Points" menu,
-    //   another child of the Editor component;  it will set state.resourceTemplateId
-    const defaultRtId = 'resourceTemplate:bf2:Monograph:Instance'
     this.state = {
-      resourceTemplateId: defaultRtId,
-      tempRtState: true
+      redirect: false
     }
   }
 
-  componentDidUpdate(nextProps){
-    if (nextProps.location.state) {
-
-      if(this.state.tempRtState){
-        const rtId = nextProps.location.state.rtId
-        const data = getResourceTemplate(rtId)
-        if (!this.state.resourceTemplateId) {
-          this.setState({resourceTemplateId: rtId})
-        }
-        this.setResourceTemplates(JSON.stringify(data))
-      }
+  componentDidUpdate(){
+    if(!this.state.redirect) {
+      this.setState({redirect: true})
     }
   }
 
-  resetTempState = () => {
-    this.setState({tempRtState: true})
-  }
-
-  //resource templates are set via StartingPoints and passed to ResourceTemplate
+  //resource templates are set via StartingPoints and passed to ResourceTemplate via redirect to Editor
   setResourceTemplates = (content) => {
-    this.setState({tempRtState: false})
     this.setState({resourceTemplateData: content})
   }
 
   render() {
-    return(
-      <div id="importResourceTemplate">
-        <Header triggerEditorMenu={this.props.triggerHandleOffsetMenu}/>
-        <StartingPoints
-          tempStateCallback={this.resetTempState}
-          resourceTemplatesCallback={this.setResourceTemplates}
-          resourceTemplateId = {this.state.resourceTemplateId}
-          setResourceTemplateCallback={this.setResourceTemplates}
-        />
-       
-      </div>
-    )
+    if (!this.state.redirect) {
+      return(
+        <div id="importResourceTemplate">
+          <Header triggerEditorMenu={this.props.triggerHandleOffsetMenu}/>
+          <StartingPoints setResourceTemplateCallback={this.setResourceTemplates} />
+        </div>
+      )
+    } else {
+      return <Redirect to={{pathname: '/editor', state: { resourceTemplateData: this.state.resourceTemplateData }}} />
+    }
   }
 }
 
@@ -64,7 +43,5 @@ ImportResourceTemplate.propTypes = {
   children: PropTypes.array,
   triggerHandleOffsetMenu: PropTypes.func
 }
-
-
 
 export default (ImportResourceTemplate)
