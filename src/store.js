6046ef8655baf040173cf326e2b76922e7af1fe9
Joshua Greben <jgreben@stanford.edu>
2019-03-20 11:47:10 -0700

    Adds an initial login state in development mode so people do not have to force a mock login

--- src/store.js
+++ src/store.js
@@ -1,10 +1,27 @@
 import { createStore } from 'redux'
 import reducer from './reducers/index'
+import Config from './Config'
 
-const store = createStore(reducer)
+let initialState = {}
+if(process.env.NODE_ENV === "development") {
+  initialState = {
+    authenticate: {
+      loginJwt: {
+        id_token: Config.awsCognitoIdToken,
+        access_token: Config.awsCognitoAccessToken,
+        username: 'test-user',
+        isAuthenticated: true,
+        expiry: 5555555555555
+    }}
+  }
+}
+
+const store = createStore(reducer, initialState)
 
 // To use the redux dev tools chrome extention, replace with:
 // const store = createStore(reducer,
-//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//   initialState,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
 
 export default store
