d106e05186653ddd22c9f2cb1e063626e0743d7f
Joshua Greben <jgreben@stanford.edu>
2019-03-21 09:15:02 -0700

    Use updated sinopia_server@3.0.0-beta2 to retrieve non-rdf resources

--- src/components/editor/SinopiaResourceTemplates.jsx
+++ src/components/editor/SinopiaResourceTemplates.jsx
@@ -1,15 +1,16 @@
 // Copyright 2019 Stanford University see Apache2.txt for license
 
 import React, {Component} from 'react'
-// import PropTypes from 'prop-types'
+import BootstrapTable from 'react-bootstrap-table-next'
 const SinopiaServer = require('sinopia_server')
 const instance = new SinopiaServer.LDPApi()
 instance.apiClient.basePath = 'http://localhost:8080'
 
-const groupPromise = new Promise((resolve) => {
+const groupPromise = new Promise((resolve, reject) => {
   resolve(instance.getBaseWithHttpInfo())
 })
-const ld4pGroupDataPromise = new Promise((resolve) => {
+
+const ld4pGroupDataPromise = new Promise((resolve, reject) => {
   resolve(instance.getGroupWithHttpInfo('ld4p'))
 })
 
@@ -19,7 +20,7 @@ class SinopiaResourceTemplates extends Component {
     this.state = {
       isLoading: false,
       groupData: [],
-      ld4pGroupData: []
+      templatesForGroup: []
     }
   }
 
@@ -31,27 +32,29 @@ class SinopiaResourceTemplates extends Component {
       } else {
         this.setState({groupData: [data.response.body.contains]})
       }
-    }, function(error) {
-      alert(error)
+    }).catch((error) => {
+      this.setState({message: error})
     })
 
+    //TODO: get resource templates for other groups
     ld4pGroupDataPromise.then((data) => {
-      this.setState({ld4pGroupData: data.response.body.contains})
-    }, function(error) {
-      alert(error)
-    })
+      let joined = []
+      data.response.body.contains.map((c) => {
+        const name = this.resourceToName(c)
 
-    // this.state.ld4pGroupData.map((resource, index) => {
-    //   const name = this.resourceToName(resource)
-    //
-    //   const promise = new Promise((resolve) => {
-    //     resolve(instance.getResourceWithHttpInfo('ld4p', name))
-    //   })
-    //
-    //   promise.then((data) => {
-    //     console.log(data)
-    //   })
-    // })
+        const promise = new Promise((resolve) => {
+          resolve(instance.getResourceWithHttpInfo('ld4p', name, { acceptEncoding: 'application/json' }))
+        })
+
+        promise.then((response_and_data) => {
+          joined.push({name: name, uri: c, id: response_and_data.response.body.id, group: 'ld4p'})
+          this.setState({templatesForGroup: joined})
+        })
+
+      })
+    }).catch((error) => {
+      this.setState({message: error})
+    })
   }
 
   resourceToName = (resource) => {
@@ -60,41 +63,43 @@ class SinopiaResourceTemplates extends Component {
   }
 
   render(){
+    const columns = [{
+      dataField: 'id',
+      text: 'Template name',
+      sort: true
+    }, {
+      dataField: 'name',
+      text: 'ID',
+      sort: true
+    }, {
+      dataField: 'group',
+      text: 'Group'
+    }];
+
+    if (this.state.message) {
+      return (
+        <div className="alert alert-warning alert-dismissible">
+          <button className="close" data-dismiss="alert" aria-label="close">&times;</button>
+          No connection to the Sinopia Server is available, or there are no resources for any group
+        </div>
+      )
+    }
+
     return(
       <div>
         <h4>Groups in Sinopia</h4>
         <ul>
           { this.state.groupData.map((container, index) => {
             return(
-              <li key={index}>{container}</li>
+              <li key={index}>{this.resourceToName(container)}</li>
             )
           })}
         </ul>
         <h4>Available Resource Templates in Sinopia</h4>
-        <ul>
-          { this.state.ld4pGroupData.map((resource, index) => {
-            const name = this.resourceToName(resource)
-
-            const promise = new Promise((resolve) => {
-              resolve(instance.getResourceWithHttpInfo('ld4p', name))
-            })
-
-            promise.then((response_and_data) => {
-              console.log(response_and_data.response)
-            })
-
-            return(
-              <li key={index}>{resource}</li>
-            )
-          })}
-        </ul>
+        <BootstrapTable keyField='id' data={ this.state.templatesForGroup } columns={ columns } />
       </div>
     )
   }
 }
-
-// SinopiaResourceTemplates.propTypes = {
-//
-// }
 
 export default (SinopiaResourceTemplates)
