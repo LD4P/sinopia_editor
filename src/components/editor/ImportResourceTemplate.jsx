372e0b10e2820b703e76d66c7bb7a4fd9b3a5cea
Michael J. Giarlo <mjgiarlo@stanford.edu>
2019-05-16 17:34:44 -0700

    Refactor resource template components
    Fixes #507

--- src/components/editor/ImportResourceTemplate.jsx
+++ src/components/editor/ImportResourceTemplate.jsx
@@ -1,6 +1,7 @@
 // Copyright 2018 Stanford University see Apache2.txt for license
 
-import React, {Component} from 'react'
+import SinopiaServer from 'sinopia_server'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Header from './Header'
 import ImportFileZone from './ImportFileZone'
@@ -8,10 +9,10 @@ import SinopiaResourceTemplates from './SinopiaResourceTemplates'
 import UpdateResourceModal from './UpdateResourceModal'
 import { loadState } from '../../localStorage'
 import Config from '../../../src/Config'
-const SinopiaServer = require('sinopia_server')
+
 const instance = new SinopiaServer.LDPApi()
 const curJwt = loadState('jwtAuth')
-const _ = require('lodash')
+
 instance.apiClient.basePath = Config.sinopiaServerBase
 if (curJwt !== undefined) {
   instance.apiClient.authentications['CognitoUser'].accessToken = curJwt.id_token
@@ -32,91 +33,92 @@ class ImportResourceTemplate extends Component {
   }
 
   componentDidMount() {
-    const incrementedKey = this.state.updateKey +1
-    this.setState({updateKey: incrementedKey})
+    const incrementedKey = this.state.updateKey + 1
+    this.setState({ updateKey: incrementedKey })
   }
 
   modalClose = () => {
     this.setState( { modalShow: false } )
   }
 
-  //resource templates are set via ImportFileZone and passed to ResourceTemplate via redirect to Editor
+  // Resource templates are set via ImportFileZone and passed to ResourceTemplate via redirect to Editor
   setResourceTemplates = (content, group) => {
     const profileCount = content.Profile.resourceTemplates.length
-    content.Profile.resourceTemplates.map(rt => {
-      this.fulfillCreateResourcePromise(this.createResourcePromise(rt, group, profileCount))
+    content.Profile.resourceTemplates.forEach(async rt => {
+      const response = await this.createResource(rt, group)
+      this.updateStateFromServerResponse(response, 'create', profileCount)
       const incrementedKey = this.state.updateKey + 1
-      this.setState({updateKey: incrementedKey})
+      this.setState({ updateKey: incrementedKey })
     })
   }
 
-  createResourcePromise = (content, group, profileCount) => new Promise(async (resolve) => {
-    resolve(
-      await instance.createResourceWithHttpInfo(group, content, { slug: content.id, contentType: 'application/json' }).catch((error) => {
-        this.updateStateForResourceError(error)
+  createResource = async (content, group) => {
+    try {
+      const response = await instance.createResourceWithHttpInfo(group, content, { slug: content.id, contentType: 'application/json' })
+      return response.response
+    } catch(error) {
+      this.setState({
+        createResourceError: [...this.state.createResourceError, error.response]
       })
-    )
-  }).then(response => {
-    if(this.state.createResourceError.length === profileCount) {
-      this.setState({modalShow: true})
-    }
-    return response
-  }).catch(() => {})
-
-  updateResourcePromise = (content, group) => new Promise(async (resolve) => {
-    resolve(
-      await instance.updateResourceWithHttpInfo(group, content.id, content, { contentType: 'application/json' }).catch((error) => {
-        this.setState({
-          updateResourceError: error
-        })
-      })
-    )
-  }).then(response => {
-
-    return response
-  }).catch(() => {})
-
-  fulfillCreateResourcePromise = async (promise) => {
-    await promise.then(result => {
-      const joinedMessages = this.state.message.slice(0)
-
-      if(result.response.statusText !== 'No Content') {
-        this.setState({tempState: `${result.response.statusText} ${result.response.headers.location}`})
-        joinedMessages.push(this.state.tempState)
-        this.setState({message: joinedMessages})
-      }
-
-    }).catch(() => {
-      if (this.state.createResourceError[0].statusText !== 'Conflict') {
-        const msg = 'The sinopia server is not accepting the request for this resource.'
-        this.state.updateResourceError ? this.setState({message: [`${msg}: ${this.state.updateResourceError}`]}) : this.setState({message: [msg]})
-      }
-    })
-  }
-
-  updateStateForResourceError = (error) => {
-    if(_.get(error, 'response.statusText') === 'Conflict') {
-      const response = error.response
-      const joinedConflicts = this.state.createResourceError.slice(0)
-      this.setState({tempConflictError: response})
-      joinedConflicts.push(this.state.tempConflictError)
-      this.setState({createResourceError: joinedConflicts})
+      return error.response
     }
   }
 
-  updateAllResources = (rts, group) => {
-    return Promise.all(rts.map(async rt =>
-      await this.fulfillCreateResourcePromise(this.updateResourcePromise(rt, group))
-    ))
+  updateResource = async (content, group) => {
+    try {
+      const response = await instance.updateResourceWithHttpInfo(group, content.id, content, { contentType: 'application/json' })
+      return response.response
+    } catch(error) {
+      this.setState({ updateResourceError: error })
+      return error.response
+    }
   }
 
-  handleUpdateResource = async (rts, group) => {
-    await this.updateAllResources(rts, group).then(() => {
-      const incrementedKey = this.state.updateKey + 1
-      this.setState({updateKey: incrementedKey})
-      this.modalClose()
-      window.location.reload()
+  updateStateFromServerResponse = (response, operation, profileCount) => {
+    // HTTP status 409 == Conflict
+    if (operation == 'update' && response.status !== 409) {
+      const msg = 'The sinopia server is not accepting the request for this resource.'
+      this.state.updateResourceError ?
+        this.setState({ message: [`${msg}: ${this.state.updateResourceError}`] }) :
+        this.setState({ message: [msg] })
+    }
+
+    // HTTP status 409 == Conflict
+    if (response.status === 409 && this.state.createResourceError.length >= profileCount) {
+      this.setState({ modalShow: true })
+    }
+
+    const location = response.headers.location || ''
+    const newMessage = `${this.humanReadableStatus(response.status)} ${location}`
+    this.setState({
+      message: [...this.state.message, newMessage]
     })
+  }
+
+  humanReadableStatus = status => {
+    switch(status) {
+      case 201:
+        return 'Created'
+      case 204:
+        return 'Updated'
+      case 401:
+        return 'You are not authorized to do this. Try logging in again!'
+      case 409:
+        return 'Attempting to update'
+      default:
+        return `Unexpected response (${status})!`
+    }
+  }
+
+  handleUpdateResource = (rts, group) => {
+    rts.forEach(async rt => {
+      const response = await this.updateResource(rt, group)
+      this.updateStateFromServerResponse(response, 'update')
+    })
+    const incrementedKey = this.state.updateKey + 1
+    this.setState({ updateKey: incrementedKey })
+    this.modalClose()
+    window.location.reload()
   }
 
   render() {
@@ -144,4 +146,4 @@ ImportResourceTemplate.propTypes = {
   triggerHandleOffsetMenu: PropTypes.func
 }
 
-export default (ImportResourceTemplate)
+export default ImportResourceTemplate
--- src/components/editor/SinopiaResourceTemplates.jsx
+++ src/components/editor/SinopiaResourceTemplates.jsx
@@ -1,13 +1,13 @@
 // Copyright 2019 Stanford University see Apache2.txt for license
 
+import SinopiaServer from 'sinopia_server'
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
 import Config from '../../../src/Config'
 import { getGroups, listResourcesInGroupContainer, getResourceTemplate } from '../../sinopiaServer'
-const _ = require('lodash')
-const SinopiaServer = require('sinopia_server')
+
 const instance = new SinopiaServer.LDPApi()
 instance.apiClient.basePath = Config.sinopiaServerBase
 
@@ -15,71 +15,68 @@ class SinopiaResourceTemplates extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: '',
-      groupData: [],
-      templatesForGroup: [],
-      contains: []
+      errors: [],
+      resourceTemplates: [],
     }
   }
 
   async componentDidUpdate(prevProps) {
-    if (this.props.updateKey > prevProps.updateKey) {
-      const groupPromise = getGroups().then(data => {
-        return data
-      }).catch(() => {})
+    if (this.props.updateKey <= prevProps.updateKey) {
+      return
+    }
 
-      await this.fulfillGroupPromise(groupPromise).then(async () => {
-        this.state.groupData.map(group => {
-          const groupName = this.resourceToName(group)
-          listResourcesInGroupContainer(groupName).then((data) => {
-            this.fulfillGroupData(data)
-          }).catch(() => {})
-        })
+    await this.fetchResourceTemplatesFromGroups()
+  }
+
+  fetchResourceTemplatesFromGroups = async () => {
+    try {
+      const getGroupsResponse = await getGroups()
+      const contains = [].concat(getGroupsResponse.response.body.contains).filter(Boolean)
+      contains.forEach(async group => {
+        const groupName = this.resourceToName(group)
+        const listResourcesInGroupResponse = await listResourcesInGroupContainer(groupName)
+        // Short-circuit listing resources in a group if it contains nothing
+        if (listResourcesInGroupResponse.response.body.contains)
+          this.setStateFromServerResponse(groupName, listResourcesInGroupResponse.response.body.contains)
       })
+    } catch(error) {
+      const errors = [...this.state.errors, error]
+      this.setState({ errors: errors })
     }
   }
 
-  fulfillGroupPromise = (promise) => {
-    promise.then(data => {
-      const contains = [].concat(data.response.body.contains)
-      this.setState({groupData: contains})
-    }).catch((error) => {
-      this.setState({message: error})
+  setStateFromServerResponse = (groupName, groupContains) => {
+    // Array-ify the value of `groupContains` as it can be either a string or an array
+    const templateIds = [].concat(groupContains)
+
+    templateIds.forEach(async templateId => {
+      const templateName = this.resourceToName(templateId)
+      const templateResponse = await getResourceTemplate(templateName, groupName)
+      const resourceTemplateBody = templateResponse.response.body
+
+      const retrievedTemplateObject = {
+        key: resourceTemplateBody.id,
+        name: resourceTemplateBody.resourceLabel,
+        uri: templateId,
+        id: resourceTemplateBody.id,
+        group: groupName
+      }
+
+      const templates = [...this.state.resourceTemplates]
+      console.dir(templates)
+      // Check if newly retrieved template is already stored in component state. If not, add it.
+      if (!templates.some(template => template.key === retrievedTemplateObject.key))
+        templates.push(retrievedTemplateObject)
+      console.dir(retrievedTemplateObject)
+      this.setState({
+        resourceTemplates: templates
+      })
     })
-    return promise
   }
 
-  fulfillGroupData = (data) => {
-    const groupName = this.resourceToName(data.response.body['@id'])
-
-    if (data.response.body.contains !== undefined) {
-      const contains = [].concat(data.response.body.contains)
-
-      contains.map(c => {
-        const name = this.resourceToName(c)
-
-        this.groupDataPromise(groupName, name).then((data) => {
-          const rt = data.response.body
-          this.setState({tempState: {key: rt.id, name: rt.resourceLabel, uri: c, id: rt.id, group: groupName}})
-          const joined = this.state.templatesForGroup.slice(0)
-          if (!_.find(joined, ['key', this.state.tempState.key])) {
-            joined.push(this.state.tempState)
-          }
-          this.setState({templatesForGroup: joined})
-        }).catch(() => {})
-      })
-    }
-
-  }
-
-  groupDataPromise = (groupName, name) => new Promise(async (resolve) => {
-    // await resolve(instance.getResourceWithHttpInfo(groupName, name, { acceptEncoding: 'application/json' }))
-    await resolve(getResourceTemplate(name, groupName))
-  })
-
-  resourceToName = (resource) => {
+  resourceToName = resource => {
     const idx = resource.lastIndexOf('/')
-    return resource.substring(idx+1)
+    return resource.substring(idx + 1)
   }
 
   linkFormatter = (cell, row) => {
@@ -88,12 +85,12 @@ class SinopiaResourceTemplates extends Component {
     )
   }
 
-  render(){
-    if (this.state.message) {
+  render() {
+    if (this.state.errors.length > 0) {
       return (
         <div className="alert alert-warning alert-dismissible">
           <button className="close" data-dismiss="alert" aria-label="close">&times;</button>
-          No connection to the Sinopia Server is available, or there are no resources for any group
+          No connection to the Sinopia Server is available, or there are no resources for any group.
         </div>
       )
     }
@@ -103,7 +100,7 @@ class SinopiaResourceTemplates extends Component {
       { this.props.message.join(', ') }
     </div>;
 
-    if(this.props.message.length === 0) {
+    if (this.props.message.length === 0) {
       createResourceMessage = <span />
     }
 
@@ -118,7 +115,7 @@ class SinopiaResourceTemplates extends Component {
       <div>
         { createResourceMessage }
         <h4>Available Resource Templates in Sinopia</h4>
-        <BootstrapTable keyField='key' data={ this.state.templatesForGroup } >
+        <BootstrapTable keyField='key' data={ this.state.resourceTemplates } >
           <TableHeaderColumn thStyle={ thNameClass } tdStyle={ tdNameClass } dataFormat={ this.linkFormatter } dataField='name' dataSort={true} >Template name</TableHeaderColumn>
           <TableHeaderColumn thStyle={ thIDClass } tdStyle={ tdIDClass } dataField='id' dataSort={true} >ID</TableHeaderColumn>
           <TableHeaderColumn thStyle={ thGroupClass } tdStyle={ tdGroupClass } dataField='group' dataSort={true} >Group</TableHeaderColumn>
