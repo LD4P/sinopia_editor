0add08a4dde308fb0de27472cbb32e2e457b733e
Justin Coyne <jcoyne@justincoyne.com>
2019-06-06 11:47:44 -0500

    Make RDFModal a purely functional component

--- src/components/editor/RDFModal.jsx
+++ src/components/editor/RDFModal.jsx
@@ -1,39 +1,35 @@
-// Copyright 2018 Stanford University see LICENSE for license
+// Copyright 2019 Stanford University see LICENSE for license
 
-import React, { Component } from 'react'
+import React from 'react'
 import Button from 'react-bootstrap/lib/Button'
 import Modal from 'react-bootstrap/lib/Modal'
 import PropTypes from 'prop-types'
 
-class RDFModal extends Component {
-  render() {
-    return (
-      <div>
-        <Modal show={this.props.show} bsSize="lg">
-          <Modal.Header>
-            <Modal.Title>
-              RDF Preview
-            </Modal.Title>
-            <div>
-              If this looks good, then click Save and Publish
-            </div>
-            <div style={{ float: 'right', marginTop: '-47px' }}>
-              <Button className="btn-link" style={{ paddingRight: '20px' }} onClick={this.props.close}>
-                Cancel
-              </Button>
-              <Button className="btn btn-primary btn-sm" onClick={ this.props.save }>
-                Save & Publish
-              </Button>
-            </div>
-          </Modal.Header>
-          <Modal.Body bsClass={'rdf-modal-content'}>
-            <pre>{this.props.rdf}</pre>
-          </Modal.Body>
-        </Modal>
-      </div>
-    )
-  }
-}
+const RDFModal = props => (
+  <div>
+    <Modal show={props.show} bsSize="lg">
+      <Modal.Header>
+        <Modal.Title>
+          RDF Preview
+        </Modal.Title>
+        <div>
+          If this looks good, then click Save and Publish
+        </div>
+        <div style={{ float: 'right', marginTop: '-47px' }}>
+          <Button className="btn-link" style={{ paddingRight: '20px' }} onClick={props.close}>
+            Cancel
+          </Button>
+          <Button className="btn btn-primary btn-sm" onClick={ props.save }>
+            Save & Publish
+          </Button>
+        </div>
+      </Modal.Header>
+      <Modal.Body bsClass={'rdf-modal-content'}>
+        <pre>{props.rdf}</pre>
+      </Modal.Body>
+    </Modal>
+  </div>
+)
 
 RDFModal.propTypes = {
   close: PropTypes.func,
