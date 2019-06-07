4e7183b5b176bfb02da460bfccc8cd095b0356bb
aaron-collier <aaron.collier@stanford.edu>
2019-06-07 13:29:51 -0700

    Fix block comment style

--- __tests__/integration/unauthenticatedNavigation.test.js
+++ __tests__/integration/unauthenticatedNavigation.test.js
@@ -7,8 +7,10 @@ describe('When an unauthenticated user tries to access resource templates', () =
     jest.setTimeout(60000) // this seems to take around 10s in practice, but be generous, just in case
     await page.goto('http://127.0.0.1:8888/templates')
 
-    // Attempt to logout
-    // This try block is necessary to avoid failing after logout
+    /*
+     * Attempt to logout
+     * This try block is necessary to avoid failing after logout
+     */
     try {
       await page.waitForSelector('button.signout-btn')
       await page.click('button.signout-btn')
