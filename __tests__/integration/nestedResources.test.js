e01b354574c7e0365280cb4b0370bf1dae5f322b
aaron-collier <aaron.collier@stanford.edu>
2019-06-18 10:45:36 -0700

    Expand nested resource test to verify content exists in preview RDF

--- __tests__/integration/nestedResources.test.js
+++ __tests__/integration/nestedResources.test.js
@@ -8,36 +8,64 @@ describe('Expanding a resource property in a property panel', () => {
     return await testUserLogin()
   })
 
-  it('loads up a resource template from the list of loaded templates', async () => {
+  it('loads up the BIBFRAME Instance resource template from the list of loaded templates', async () => {
     expect.assertions(2)
     await pupExpect(page).toClick('a', { text: 'BIBFRAME Instance' })
     await pupExpect(page).toMatch('BIBFRAME Instance')
   })
 
-  it('clicks on one of the property type rows to expand a nested resource', async () => {
+  it('clicks on the Has BIBFRAME Instance property type rows to expand the resource', async () => {
     expect.assertions(2)
     await pupExpect(page).toClick('a[data-id=\'hasInstance\']')
     await pupExpect(page).toMatchElement('h5', { text: 'BIBFRAME Instance' })
   })
 
-  it('clicks on a nested property to reveal an input component', async () => {
-    expect.assertions(2)
-    await pupExpect(page).toClick('a[data-id=\'heldBy\']')
-    await pupExpect(page).toMatchElement('input[placeholder=\'Holdings\']')
-  })
-
-  it('enters a value into a nested prooperty component', async () => {
+  it('clicks on the Title property to reveal the main title input component', async () => {
     expect.assertions(3)
-    await pupExpect(page).not.toMatchElement('div#userInput', { text: 'Some text' })
-    await expect(page).toFill('input[placeholder="Holdings"', 'Some text')
-    await page.keyboard.press('Enter')
-    await pupExpect(page).toMatchElement('div#userInput', { text: 'Some text' })
+    await pupExpect(page).toClick('a[data-id=\'title\']')
+    await pupExpect(page).toClick('a[data-id=\'mainTitle\']')
+    await pupExpect(page).toMatchElement('input[placeholder=\'Preferred Title for Work (RDA 6.2.2, RDA 6.14.2) (BIBFRAME: Main title)\']')
   })
 
-  it('enters a non-roman value into a nested property component', async () => {
-    expect.assertions(2)
-    await pupExpect(page).toFill('input[placeholder="Holdings"', '甲骨文')
+  it('enters a title into the BIBFRAME Instance Work Title component', async () => {
+    expect.assertions(3)
+    await pupExpect(page).not.toMatchElement('div#userInput', { text: 'An interesting title' })
+    await expect(page).toFill('input[placeholder="Preferred Title for Work (RDA 6.2.2, RDA 6.14.2) (BIBFRAME: Main title)"', 'An interesting title')
     await page.keyboard.press('Enter')
-    await pupExpect(page).toMatchElement('div#userInput', { text: '甲骨文' })
+    await pupExpect(page).toMatchElement('div#userInput', { text: 'An interesting title' })
+  })
+
+  it('clicks on the Responsibility Statement property to reveal the responsibility input component', async () => {
+    expect.assertions(2)
+    await pupExpect(page).toClick('a[data-id=\'responsibilityStatement\']')
+    await pupExpect(page).toMatchElement('input[placeholder=\'Statement of Responsibility Relating to Title Proper (RDA 2.4.2)\']')
+  })
+
+  it('enters text into the BIBFRAME Instance Statement of Responsibility component', async () => {
+    expect.assertions(3)
+    await pupExpect(page).not.toMatchElement('div#userInput', { text: 'A statement of responsibility' })
+    await expect(page).toFill('input[placeholder="Statement of Responsibility Relating to Title Proper (RDA 2.4.2)"', 'A statement of responsibility')
+    await page.keyboard.press('Enter')
+    await pupExpect(page).toMatchElement('div#userInput', { text: 'A statement of responsibility' })
+  })
+
+  it('clicks on Contribution property to reveal the input component', async () => {
+    expect.assertions(6)
+    await pupExpect(page).toClick('a[data-id=\'contribution\']')
+    await pupExpect(page).toMatchElement('input[placeholder=\'Agent Contribution\']')
+    await expect(page).toFill('input[placeholder=\'Agent Contribution\']', 'Stanford family')
+    await pupExpect(page).toMatchElement('ul#lookupComponent')
+    await page.waitForSelector('li#rbt-menu-item-0')
+    await pupExpect(page).toMatchElement('li#rbt-menu-item-0')
+    await pupExpect(page).toClick('li#rbt-menu-item-0')
+  })
+
+  it('it clicks on Preview RDF and verifies the text', async () => {
+    expect.assertions(4)
+    await pupExpect(page).toClick('button', { text: 'Preview RDF' })
+    const rdfOut = await page.$eval('pre', e => e.textContent) // Copy the text from the modal into a variable
+    await expect(rdfOut).toMatch('An interesting title')
+    await expect(rdfOut).toMatch('A statement of responsibility')
+    await expect(rdfOut).toMatch('http://id.loc.gov/authorities/subjects/sh85127327') // this is the Stanford family URI
   })
 })
