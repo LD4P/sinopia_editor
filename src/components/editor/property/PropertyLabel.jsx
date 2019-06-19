baeb2bbc940951af0439f5709f2699941ebd656d
Joshua Greben <jgreben@stanford.edu>
2019-06-19 14:51:11 -0700

    fix linting error

--- src/components/editor/property/PropertyLabel.jsx
+++ src/components/editor/property/PropertyLabel.jsx
@@ -5,6 +5,7 @@ import React, { Component } from 'react'
 import { OverlayTrigger, Popover } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import RequiredSuperscript from './RequiredSuperscript'
+
 const _ = require('lodash')
 
 export class PropertyLabel extends Component {
