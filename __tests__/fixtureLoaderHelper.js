fcb579723c30b89de92e0e7ab06df6d55d56c3b2
Naomi Dushay <ndushay@stanford.edu>
2019-06-21 10:55:28 -0700

    simplify fixture loading

--- __tests__/fixtureLoaderHelper.js
+++ __tests__/fixtureLoaderHelper.js
@@ -1,56 +1,62 @@
 // Copyright 2018, 2019 Stanford University see LICENSE for license
 
-const onlyEquivalentRt = require('./__fixtures__/OnlyEquivalent.json')
-const barcodeRt = require('./__fixtures__/Barcode.json')
-const monographInstanceRt = require('./__fixtures__/MonographInstance.json')
-const monographWorkRt = require('./__fixtures__/MonographWork.json')
-const noteRt = require('./__fixtures__/Note.json')
-const parallelTitleRt = require('./__fixtures__/ParallelTitle.json')
-const titleRt = require('./__fixtures__/Title.json')
-const titleNoteRt = require('./__fixtures__/TitleNote.json')
-const transcribedTitleRt = require('./__fixtures__/TranscribedTitle.json')
-const varTitleRt = require('./__fixtures__/VarTitle.json')
-const workTitleRt = require('./__fixtures__/WorkTitle.json')
-const workVariantTitleRt = require('./__fixtures__/WorkVariantTitle.json')
-const lccnRt = require('./__fixtures__/LCCN.json')
-const ddcRt = require('./__fixtures__/DDC.json')
-const shelfMarkRt = require('./__fixtures__/Shelfmark.json')
-const itemRt = require('./__fixtures__/Item.json')
-const retentionRt = require('./__fixtures__/ItemRetention.json')
-const itemAcqSourceRt = require('./__fixtures__/ItemAcqSource.json')
-const enumerationRt = require('./__fixtures__/ItemEnumeration.json')
-const chronologyRt = require('./__fixtures__/ItemChronology.json')
-const adminMetaRt = require('./__fixtures__/adminMetadata.json')
-const adminMetaStatusRt = require('./__fixtures__/adminMetadataStatus.json')
-const rdaItemMonoRt = require('./__fixtures__/rdaItemMonograph.json')
-const rdaManifestMonoRt = require('./__fixtures__/RDAMainifestationMonograph.json')
+// ideally we would like to automagically load everything in the fixtures dir
+//  (with a short black list), but because we want to be able to spin up the
+//  web server with fixtures, we can't use fs.readdir, fs.readfile and so on.
+// Using require to load the json files gets around this, but only if we have
+//  the names of the files in a list already.
 
-export const resourceTemplateId2Json = [
-  { id: 'resourceTemplate:bf2:Monograph:Instance', json: monographInstanceRt },
-  { id: 'resourceTemplate:bf2:Monograph:Work', json: monographWorkRt },
-  { id: 'resourceTemplate:bf2:Identifiers:Barcode', json: barcodeRt },
-  { id: 'resourceTemplate:bf2:OnlyEquivalent', json: onlyEquivalentRt },
-  { id: 'resourceTemplate:bf2:Note', json: noteRt },
-  { id: 'resourceTemplate:bf2:ParallelTitle', json: parallelTitleRt },
-  { id: 'resourceTemplate:bf2:Title', json: titleRt },
-  { id: 'resourceTemplate:bf2:Title:Note', json: titleNoteRt },
-  { id: 'resourceTemplate:bflc:TranscribedTitle', json: transcribedTitleRt },
-  { id: 'resourceTemplate:bf2:Title:VarTitle', json: varTitleRt },
-  { id: 'resourceTemplate:bf2:WorkTitle', json: workTitleRt },
-  { id: 'resourceTemplate:bf2:WorkVariantTitle', json: workVariantTitleRt },
-  { id: 'resourceTemplate:bf2:Identifiers:LCCN', json: lccnRt },
-  { id: 'resourceTemplate:bf2:Identifiers:DDC', json: ddcRt },
-  { id: 'resourceTemplate:bf2:Identifiers:Shelfmark', json: shelfMarkRt },
-  { id: 'resourceTemplate:bf2:Item', json: itemRt },
-  { id: 'resourceTemplate:bf2:Item:Retention', json: retentionRt },
-  { id: 'resourceTemplate:bf2:Item:ItemAcqSource', json: itemAcqSourceRt },
-  { id: 'resourceTemplate:bf2:Item:Enumeration', json: enumerationRt },
-  { id: 'resourceTemplate:bf2:Item:Chronology', json: chronologyRt },
-  { id: 'rt:bf2:AdminMetadata', json: adminMetaRt },
-  { id: 'rt:bf2:AdminMetadata:Status', json: adminMetaStatusRt },
-  { id: 'rt:rda:item:monograph', json: rdaItemMonoRt },
-  { id: 'rt:rda:manifestation:monograph', json: rdaManifestMonoRt },
+// note that the following fixtures are used for schemaValidation tests and don't need to be autoloaded
+// 'ddc_bad_json.json',
+// 'edition_bad_schema',
+// 'lcc_no_schema_specified',
+// 'lcc_v0.0.9.json',
+// 'lcc_v0.2.0.bad_id.json',
+// 'lcc_v0.2.0_invalid.json',
+// 'lcc_v0.2.0.json',
+// 'Note(Profile).json', // detritus from "spoof" that could be removed?
+// 'place_profile_no_schema_specified',
+// 'place_profile_v0.0.9.json',
+// 'place_profile_v0.2.0.json',
+
+const rtFileNames = [
+  'Barcode.json',
+  'DDC.json',
+  'Item.json',
+  'ItemAcqSource.json',
+  'ItemChronology.json',
+  'ItemEnumeration.json',
+  'ItemRetention.json',
+  'LCCN.json',
+  'MonographInstance.json',
+  'MonographWork.json',
+  'Note.json',
+  'OnlyEquivalent.json',
+  'ParallelTitle.json',
+  'RDAMainifestationMonograph.json',
+  'Shelfmark.json',
+  'Title.json',
+  'TitleNote.json',
+  'TranscribedTitle.json',
+  'VarTitle.json',
+  'WorkTitle.json',
+  'WorkVariantTitle.json',
+  'adminMetadata.json',
+  'adminMetadataStatus.json',
+  'rdaItemMonograph.json',
 ]
+
+export const resourceTemplateId2Json = loadFixtureResourceTemplates()
+
+function loadFixtureResourceTemplates() {
+  const result = []
+  rtFileNames.forEach((filename) => {
+    /* eslint security/detect-non-literal-require: 'off' */
+    const rtjson = require(`./__fixtures__/${filename}`)
+    result.push({ id: rtjson.id, json: rtjson })
+  })
+  return result
+}
 
 const emptyTemplate = { propertyTemplates: [{}] }
 
