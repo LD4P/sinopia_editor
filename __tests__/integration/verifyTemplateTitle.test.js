5ce53a27e1e3d6c1b73d6289414452224d77e027
aaron-collier <aaron.collier@stanford.edu>
2019-06-27 15:24:18 -0700

    Add a small integration test to verify the title is displayed

---
+++ __tests__/integration/verifyTemplateTitle.test.js
@@ -0,0 +1,19 @@
+// Copyright 2019 Stanford University see LICENSE for license
+
+import pupExpect from 'expect-puppeteer'
+import { testUserLogin } from './loginHelper'
+import { fillInRequredFieldsForBibframeInstance } from './previewRDFHelper'
+
+describe('Verify the resource template title is displayed in the editor', () => {
+  beforeAll(async () => {
+    await testUserLogin()
+  })
+
+  it('verifies that the title is in the header', async () => {
+    expect.assertions(2)
+
+    await pupExpect(page).toClick('a', { text: 'Note' })
+    await pupExpect(page).toMatchElement('h1', { text: 'Note' })
+
+  })
+})
