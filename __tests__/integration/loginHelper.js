d83ff80769c7a00433eedbc1c5646fabb31cc59c
aaron-collier <aaron.collier@stanford.edu>
2019-07-05 16:59:32 -0700

    Capture the QA Lookup in this integration test to avoid the network requirement

--- __tests__/integration/loginHelper.js
+++ __tests__/integration/loginHelper.js
@@ -2,8 +2,25 @@
 
 import Config from 'Config'
 
+// The qaResponse is the single `Stanford family` result that we are checking for in the integration test
+const qaResponse = [{ uri: 'http://id.loc.gov/authorities/subjects/sh85127327', id: 'http://id.loc.gov/authorities/subjects/sh85127327', label: 'Stanford family' }]
+
 export async function testUserLogin() {
   jest.setTimeout(60000) // Takes around 10s in practice, but be generous, just in case
+
+  // This captures the requests sent to the authorities endpoint at lookup.ld4l.org in order
+  // To avoid depending on network services during integration testing.
+  await page.setRequestInterception(true)
+  page.on('request', (interceptedRequest) => {
+    if (interceptedRequest.url().includes('lookup.ld4l.org/authorities/search/linked_data/locsubjects_ld4l_cache/person')) {
+      interceptedRequest.respond({
+        status: 200,
+        contentType: 'application/json; charset=utf-8',
+        body: JSON.stringify(qaResponse),
+      })
+    } else interceptedRequest.continue() // If the url isn't the specific one above, pass the request on
+  })
+
   await page.goto('http://127.0.0.1:8888/templates')
 
   try {
