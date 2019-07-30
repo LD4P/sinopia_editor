fe2703c14e5d8474f77c10ffb29c09713059c9bd
Justin Littman <justinlittman@stanford.edu>
2019-07-30 09:49:54 -0400

    Tweak to integration test.

--- __tests__/integration/previewRDFHelper.js
+++ __tests__/integration/previewRDFHelper.js
@@ -3,20 +3,27 @@
 import pupExpect from 'expect-puppeteer'
 
 export async function fillInRequredFieldsForBibframeInstance() {
+  await page.setViewport({
+    width: 1822,
+    height: 961,
+  })
   // This will add 1 assertion to each it block's assertion count
   await pupExpect(page).toClick('a', { text: 'BIBFRAME Instance' })
+  await page.waitForSelector('a', { text: 'Editor' })
 
   // Click on one of the property type rows to expand a nested resource
-  await page.waitForSelector('button.btn-add[data-id=\'title\']')
-  await page.click('button.btn-add[data-id=\'title\']')
-  await page.waitForSelector('button[data-id=\'mainTitle\']')
+  await page.waitForSelector('div.rOutline-header button.btn-add[data-id=\'title\']')
+  await page.click('div.rOutline-header button.btn-add[data-id=\'title\']')
 
   // Fill in required element
-  await page.click('button.btn-add[data-id=\'mainTitle\']')
-  await page.type('[placeholder=\'Preferred Title for Work (RDA 6.2.2, RDA 6.14.2) (BIBFRAME: Main title)\']', 'Hello')
+  await page.waitForSelector('button.btn-add[data-id=\'partName\']')
+  await page.click('button.btn-add[data-id=\'partName\']')
+  await page.waitForSelector('input[placeholder=\'Part name\']')
+  await page.type('input[placeholder=\'Part name', 'Hello')
   await page.keyboard.press('Enter')
 
   // Fill in required element
+  await page.waitForSelector('[placeholder=\'Statement of Responsibility Relating to Title Proper (RDA 2.4.2)\'', 'World')
   await page.type('[placeholder=\'Statement of Responsibility Relating to Title Proper (RDA 2.4.2)\'', 'World')
   await page.keyboard.press('Enter')
 
