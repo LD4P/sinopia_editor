f47927b214ca1d5a6cf3d224a34de0ff0e1f1fd4
Justin Littman <justinlittman@stanford.edu>
2019-10-11 13:56:19 -0400

    Eslint fixes to NewsItem.

--- src/components/home/NewsItem.jsx
+++ src/components/home/NewsItem.jsx
@@ -7,9 +7,11 @@ const NewsItem = () => (
   <div className="news-item">
     <h1>Sinopia Version {version} fixes critical bugs</h1>
     <ul>
-      <li>From Search tab when you try to open a Resource created with a Resource Template that no longer exists, you will get a message explaining why the Resource can't be opened.
+      <li>From Search tab when you try to open a Resource created with a Resource
+        Template that no longer exists, you will get a message explaining why the Resource cannot be opened.
       </li>
-      <li>From Resource Template tab, Sinopia now checks for problems with a Resource Template before opening, and reports those problems, including: nesting another Resource Template that doesn't exist, or referring to a lookup source that is not supported.
+      <li>From Resource Template tab, Sinopia now checks for problems with a Resource Template before opening,
+        and reports those problems, including: nesting another Resource Template that does not exist, or referring to a lookup source that is not supported.
       </li>
       <li>Fixes issue where Resources were not getting saved.</li>
       <li>New feature: Lookup fields now support having multiple sources that are direct lookups to id.loc.gov.</li>
