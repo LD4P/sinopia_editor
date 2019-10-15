a275a38a4e3c4a1b1154583582dca3e3c3cdd2d3
Justin Coyne <jcoyne@justincoyne.com>
2019-10-15 10:50:25 -0500

    Convert DropZone to a functional component

--- src/components/templates/DropZone.jsx
+++ src/components/templates/DropZone.jsx
@@ -1,70 +1,45 @@
 // Copyright 2019 Stanford University see LICENSE for license
 
-import React, { Component } from 'react'
+import React from 'react'
 import Dropzone from 'react-dropzone'
 import PropTypes from 'prop-types'
 
-class DropZone extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
+const DropZone = (props) => {
+  const handleOnDrop = (files) => {
+    props.setGroupCallback(props.defaultSinopiaGroupId)
+    props.dropFileCallback(files)
   }
 
-  /*
-   * TODO: When we need to let the user select a group:
-   * handleChange = (event) => {
-   *   this.props.setGroupCallback(event.target.value)
-   *   this.setState({group: event.target.value})
-   * }
-   */
-  handleOnDrop = (files) => {
-    this.props.setGroupCallback(this.props.defaultSinopiaGroupId)
-    this.props.dropFileCallback(files)
+  const fileName = {
+    fontSize: '18px',
+  }
+  const listStyle = {
+    listStyleType: 'none',
   }
 
-  render() {
-    const fileName = {
-      fontSize: '18px',
-    }
-    const listStyle = {
-      listStyleType: 'none',
-    }
-    // TODO: fetch all the existing groups from trellis or a config source and render the select form:
-
-    return (
-      <section>
-        {/* <strong> */}
-        {/* 1.) Pick your Sinopia group: */}
-        {/* </strong> */}
-        {/* <form style={{paddingTop: '10px'}}> */}
-        {/* <select value={this.state.group} onChange={this.handleChange}> */}
-        {/*
-          * If we need to have the user select a group, see e.g. select form in GroupChoiceModal
-          * */}
-        {/* </select> */}
-        {/* </form> */}
-        <strong>
-          Drag and drop a resource template file in the box
-          <div style={{ paddingLeft: '20px' }}>
-            or click it to select a file to upload:
-          </div>
-        </strong>
-        <div className="DropZone" style={{ paddingTop: '20px', paddingLeft: '20px' }}>
-          <Dropzone
-            onFileDialogCancel={() => this.props.showDropZoneCallback(false)}
-            onDrop={this.handleOnDrop.bind(this)}
-            multiple={false}
-          />
-          <aside>
-            <h5>Loaded resource template file:</h5>
-            <ul style={listStyle}>
-              { this.props.filesCallback.map(f => <li style={fileName} key={f.name}>{f.name} - {f.size} bytes</li>) }
-            </ul>
-          </aside>
+  return (
+    <section>
+      <strong>
+        Drag and drop a resource template file in the box
+        <div style={{ paddingLeft: '20px' }}>
+          or click it to select a file to upload:
         </div>
-      </section>
-    )
-  }
+      </strong>
+      <div className="DropZone" style={{ paddingTop: '20px', paddingLeft: '20px' }}>
+        <Dropzone
+          onFileDialogCancel={() => props.showDropZoneCallback(false)}
+          onDrop={handleOnDrop.bind(this)}
+          multiple={false}
+        />
+        <aside>
+          <h5>Loaded resource template file:</h5>
+          <ul style={listStyle}>
+            { props.filesCallback.map(f => <li style={fileName} key={f.name}>{f.name} - {f.size} bytes</li>) }
+          </ul>
+        </aside>
+      </div>
+    </section>
+  )
 }
 
 
