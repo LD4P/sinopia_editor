df1fe8f350cada680002a55692e1efcca002aec5
Justin Coyne <jcoyne@justincoyne.com>
2019-11-04 14:55:53 -0600

    Remove unused params

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -53,12 +53,6 @@ const Editor = (props) => {
 
 Editor.propTypes = {
   triggerHandleOffsetMenu: PropTypes.func,
-  userWantsToSave: PropTypes.func,
-  copyToNewResource: PropTypes.func,
-  isSaved: PropTypes.bool,
-  location: PropTypes.object,
-  history: PropTypes.object,
-  currentUser: PropTypes.object,
   isMenuOpened: PropTypes.bool,
 }
 
