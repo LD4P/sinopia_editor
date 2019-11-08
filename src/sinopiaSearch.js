e1a1d43e24823d08e8446b59c7f88fed5b7553be
Jeremy Nelson <jermnelson@gmail.com>
2019-11-08 09:44:57 -0700

    Now returns all responses >= 300, fixes regular Sinopia search result as well

--- src/sinopiaSearch.js
+++ src/sinopiaSearch.js
@@ -22,7 +22,16 @@ export const getSearchResults = async (query, queryFrom = 0, size = Config.searc
   const url = `${Config.searchHost}${Config.searchPath}`
 
   return fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body) })
-    .then(resp => resp.json())
+    .then((resp) => {
+      if (resp.status >= 300) {
+        return {
+          totalHits: 0,
+          results: [],
+          error: `${resp.status}: ${resp.statusText}`,
+        }
+      }
+      return resp.json()
+    })
     .then((json) => {
       if (json.error) {
         return {
@@ -72,7 +81,7 @@ export const getTemplateSearchResults = async (query) => {
   const url = `${Config.searchHost}${Config.templateSearchPath}`
   return fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body) })
     .then((resp) => {
-      if (resp.status > 300) {
+      if (resp.status >= 300) {
         return {
           totalHits: 0,
           results: [],
