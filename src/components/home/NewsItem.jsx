65420923af82e773bfaa4c93511c9cc0c8300001
michelleif <23069165+michelleif@users.noreply.github.com>
2019-12-06 07:23:39 -0800

    Update NewsItem.jsx

--- src/components/home/NewsItem.jsx
+++ src/components/home/NewsItem.jsx
@@ -9,11 +9,10 @@ const NewsItem = () => (
     <h4>Sinopia Version {version} highlights</h4>
     <p><i>For complete release notes see the <a href="https://github.com/LD4P/sinopia/wiki/Latest-Release,-What's-Next">Sinopia help site</a>.</i></p>
     <ul>
-      <li>On Search tab, filter your Sinopia search results by resource class and by group.</li>
-      <li>&quot;Create New&quot; dropdown menu on Sinopia lookup fields displays entire template label and ID.</li>
-      <li>In lookup fields with multiple sources, select which of the sources you want results from.</li>
-      <li>On Resource Templates tab, page through additional items.</li>
-      <li>On Edit tab, number of columns expands as you widen browser.</li>
+      <li>Component for easier entering of diacritics for literal fields.</li>
+      <li>&quot;Create New&quot; dropdown menu on Sinopia lookp fields shows all matching items.</li>
+      <li>Images are included in extended context in Search results.</li>
+      <li>URI of resource is displayed in Search results for Sinopia and for external sources</li>
     </ul>
   </div>
 )
