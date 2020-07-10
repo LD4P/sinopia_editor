0a46ca89c8bd34e1581780b3ab95776ef5d75cef
Justin Littman <justinlittman@stanford.edu>
2020-07-10 16:22:04 -0400

    Added test for template selectors.
    closes #2254

---
+++ __tests__/selectors/templates.test.js
@@ -0,0 +1,32 @@
+import { createState } from 'stateUtils'
+import { selectHistoricalTemplates, selectSubjectAndPropertyTemplates } from 'selectors/templates'
+
+describe('selectHistoricalTemplates()', () => {
+  it('returns empty when no historical templates', () => {
+    const state = createState()
+    expect(selectHistoricalTemplates(state)).toHaveLength(0)
+  })
+
+  it('returns templates', () => {
+    const state = createState({ hasCurrentResource: true })
+    state.selectorReducer.historicalTemplates = ['ld4p:RT:bf2:Title:AbbrTitle']
+
+    const templates = selectHistoricalTemplates(state)
+    expect(templates).toHaveLength(1)
+    expect(templates[0]).toBeSubjectTemplate('ld4p:RT:bf2:Title:AbbrTitle')
+  })
+})
+
+describe('selectSubjectAndPropertyTemplates()', () => {
+  it('returns null when no subject', () => {
+    const state = createState()
+    expect(selectSubjectAndPropertyTemplates(state, 'abc123')).toEqual([null, []])
+  })
+
+  it('returns templates', () => {
+    const state = createState({ hasCurrentResource: true })
+    const [subjectTemplate, propertyTemplates] = selectSubjectAndPropertyTemplates(state, 'ld4p:RT:bf2:Title:AbbrTitle')
+    expect(subjectTemplate).toBeSubjectTemplate('ld4p:RT:bf2:Title:AbbrTitle')
+    expect(propertyTemplates).toBePropertyTemplates(['ld4p:RT:bf2:Title:AbbrTitle > http://id.loc.gov/ontologies/bibframe/mainTitle'])
+  })
+})
