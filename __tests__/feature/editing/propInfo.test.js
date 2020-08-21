b72c8678cb4b5236296b5419f715efb95fe39568
Joshua Greben <jgreben@stanford.edu>
2020-08-21 14:39:31 -0700

    Adds a test to check the rendering of the property remark into a tooltip or a link

---
+++ __tests__/feature/editing/propInfo.test.js
@@ -0,0 +1,36 @@
+import { renderApp, createHistory } from 'testUtils'
+import { screen } from '@testing-library/react'
+
+// Mock jquery
+global.$ = jest.fn().mockReturnValue({ popover: jest.fn() })
+
+describe('getting property related info from a resorce', () => {
+  it('has tooltip text info or a link based on the content of a top-level property remark', async () => {
+    const history = createHistory(['/editor/resourceTemplate:testing:uber3'])
+    renderApp(null, history)
+
+    // // if the tooltip remark is text
+    const infoIcon = await screen.findByTitle('Uber template3, property1')
+    expect(infoIcon).toHaveAttribute('data-content', 'A literal')
+
+    // // if the remark us a Url
+    const infoLink = await screen.findByRole('link', { name: 'http://access.rdatoolkit.org/1.0.html' })
+    expect(infoLink).toHaveClass('prop-remark')
+  })
+
+  it('has tooltip text info based on the content of a nested property remark', async () => {
+    const history = createHistory(['/editor/resourceTemplate:testing:uber1'])
+    renderApp(null, history)
+
+    // Finds the parent property
+    const infoIcon = await screen.findByTitle('Uber template1, property18')
+    expect(infoIcon).toHaveAttribute('data-content', 'Mandatory nested resource templates.')
+
+    // Finds the nested resource
+    await screen.findByRole('heading', { name: 'Uber template4' })
+
+    // Finds the nested property info (tooltip remark is text)
+    const nestedInfoIcon = await screen.findByTitle('Uber template4, property1')
+    expect(nestedInfoIcon).toHaveAttribute('data-content', 'A repeatable, required literal')
+  })
+})
