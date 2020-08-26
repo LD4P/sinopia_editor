6a754870b6bd6bb22b992b5a4168af1bc6806b62
Johnathan Martin <john.martin@stanford.edu>
2020-08-26 01:03:05 -0700

    somewhat more roundtrip test of EXPORTS_RECEIVED action

--- __tests__/reducers/exports.test.js
+++ __tests__/reducers/exports.test.js
@@ -1,6 +1,12 @@
 // Copyright 2020 Stanford University see LICENSE for license
 import { exportsReceived } from 'reducers/exports'
+import { createReducer } from 'reducers/index'
 import { createState } from 'stateUtils'
+
+const handlers = {
+  EXPORTS_RECEIVED: exportsReceived,
+}
+const reducer = createReducer(handlers)
 
 describe('exportsReceived', () => {
   const exportFilenames = [
@@ -9,7 +15,13 @@ describe('exportsReceived', () => {
   ]
 
   it('sets the list of export file names', () => {
-    const newState = exportsReceived(createState().selectorReducer, { payload: exportFilenames })
+    const action = {
+      type: 'EXPORTS_RECEIVED',
+      payload: exportFilenames,
+    }
+
+    const oldState = createState().selectorReducer
+    const newState = reducer(oldState, action)
     expect(newState).toMatchObject({
       entities: {
         exports: exportFilenames,
