bfaafe49410c3a47edaef17c42420e04396b01e0
Justin Littman <justinlittman@stanford.edu>
2020-09-10 15:19:33 -0400

    Removes detritus.
    closes #2337

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ import express from 'express'
 import request from 'request'
 import bodyParser from 'body-parser'
 import Config from './src/Config'
-import versoSpoof from './src/versoSpoof'
 import _ from 'lodash'
 import cors from 'cors'
 
@@ -54,35 +53,6 @@ app.post('/api/search/:index/sinopia/_search', (req, res) => {
 
 // Serve static assets to the browser, e.g., from src/styles/ and static/
 app.use(express.static(`${__dirname}/`))
-
-app.all('/verso/api/configs', (req, res, next) => {
-  if (req.query.filter.where.configType === 'profile') {
-    res.json(versoSpoof.profiles)
-  } else if (req.query.filter.where.configType === 'ontology') {
-    res.json(versoSpoof.ontologies)
-  } else {
-    res.status(400).send(`Verso not enabled -- app made request ${req.originalUrl}`)
-  }
-  next()
-})
-
-app.all('/profile-edit/server/whichrt', (req, res) => {
-  const reqUri = req.query.uri
-
-  if (reqUri !== null) {
-    if (versoSpoof.ontologyUrls.includes(reqUri)) {
-      const json = versoSpoof.owlOntUrl2JsonMappings.find((el) => reqUri === el.url).json
-
-      res.json(json)
-    } else {
-      console.error(`/profile-edit/server/whichrt called for uri other than ontology ${reqUri}`)
-      res.status(400).send(`/profile-edit/server/whichrt called for uri other than ontology ${reqUri}`)
-    }
-  } else {
-    console.error(`/profile-edit/server/whichrt called without uri ${req.originalUrl}`)
-    res.status(400).send(`/profile-edit/server/whichrt called without uri ${req.originalUrl}`)
-  }
-})
 
 app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/index.html`)
--- static/cachedHttp/id.loc.gov/ontologies/bibframe/README.md
+++
@@ -1,2 +0,0 @@
-### 2018-11-05
-Initial retrieval of ontologies from http://id.loc.gov/ontologies/bibframe
