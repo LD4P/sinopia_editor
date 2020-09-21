2c78a47c79707b095e1e99ced89b72bee579e258
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-09-21 16:24:05 -0700

    Do not add root resource template ID to history
    Fixes #2510

--- src/reducers/templates.js
+++ src/reducers/templates.js
@@ -1,11 +1,18 @@
 // Copyright 2019 Stanford University see LICENSE for license
 
+import Config from 'Config'
+
 // Keeps a unique list of templates limited to 7
 export const addTemplateHistory = (state, action) => {
   const newState = { ...state }
-  if (newState.historicalTemplates.indexOf(action.payload) !== -1) return newState
+  const template = action.payload
 
-  newState.historicalTemplates = [...state.historicalTemplates, action.payload].slice(-7)
+  if (newState.historicalTemplates.indexOf(template) !== -1
+      || template === Config.rootResourceTemplateId) {
+    return newState
+  }
+
+  newState.historicalTemplates = [...state.historicalTemplates, template].slice(-7)
   return newState
 }
 
