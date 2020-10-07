72547bda59bc0066b624744fd11df483060fcdb6
Justin Littman <justinlittman@stanford.edu>
2020-10-06 21:09:24 -0400

    Sorts Input Lists.
    refs #2616

--- __tests__/reducers/lookups.test.js
+++ __tests__/reducers/lookups.test.js
@@ -6,21 +6,34 @@ describe('lookupOptionsRetrieved', () => {
   const uri = 'https://id.loc.gov/vocabulary/mgroove'
   const lookup = [
     {
-      id: 'mDg4LzQtGH',
-      label: 'Coarse groove',
-      uri: 'http://id.loc.gov/vocabulary/mgroove/coarse',
-    },
-    {
       id: 'sFdbC6NLsZ',
       label: 'Lateral or combined cutting',
       uri: 'http://id.loc.gov/vocabulary/mgroove/lateral',
+    },
+    {
+      id: 'mDg4LzQtGH',
+      label: 'Coarse groove',
+      uri: 'http://id.loc.gov/vocabulary/mgroove/coarse',
     },
   ]
 
   it('adds a new lookup', () => {
     const newState = lookupOptionsRetrieved(createState().entities, { payload: { uri, lookup } })
     expect(newState).toMatchObject({
-      lookups: { [uri]: lookup },
+      lookups: {
+        [uri]: [
+          {
+            id: 'mDg4LzQtGH',
+            label: 'Coarse groove',
+            uri: 'http://id.loc.gov/vocabulary/mgroove/coarse',
+          },
+          {
+            id: 'sFdbC6NLsZ',
+            label: 'Lateral or combined cutting',
+            uri: 'http://id.loc.gov/vocabulary/mgroove/lateral',
+          },
+        ],
+      },
     })
   })
 })
--- src/reducers/lookups.js
+++ src/reducers/lookups.js
@@ -6,12 +6,24 @@
  * @param {Object} action to be performed
  * @return {Object} the next redux state
  */
-export const lookupOptionsRetrieved = (state, action) => ({
-  ...state,
-  lookups: {
-    ...state.lookups,
-    [action.payload.uri]: action.payload.lookup,
-  },
-})
+export const lookupOptionsRetrieved = (state, action) => {
+  const lookups = [...action.payload.lookup]
+  lookups.sort((item1, item2) => {
+    if (item1.label < item2.label) {
+      return -1
+    }
+    if (item1.label > item2.label) {
+      return 1
+    }
+    return 0
+  })
+  return {
+    ...state,
+    lookups: {
+      ...state.lookups,
+      [action.payload.uri]: lookups,
+    },
+  }
+}
 
 export const noop = () => {}
