11aea8a672878661e2c6e9a4ad8d799968a35780
Peter Mangiafico <peter@mangiafico.org>
2021-09-29 16:33:42 -0700

    refactor radio button values; add comments

--- src/components/editor/inputs/InputLang.jsx
+++ src/components/editor/inputs/InputLang.jsx
@@ -17,35 +17,45 @@ import { selectLanguages, hasLanguages } from "selectors/languages"
  * See ISO 639 for the list of registered language codes
  */
 const InputLang = (props) => {
+  const noLangSelected = "absent" // the values of the radio buttons for when a language is selected or not
+  const langSelected = "present"
+
   const [lang, setLang] = useState(props.lang)
   const [submitEnabled, setSubmitEnabled] = useState(true)
   const [radioButtonValue, setRadioButtonValue] = useState(
-    props.lang === null ? "absent" : "present"
+    props.lang === null ? noLangSelected : langSelected
   )
 
   const classes = ["modal", "fade"]
   let display = "none"
   const alertClasses = ["alert", "alert-danger"]
-  if (submitEnabled) alertClasses.push("hidden")
+  if (submitEnabled) alertClasses.push("hidden") // hide the alert if the submit button is enabled
 
   if (props.show) {
     classes.push("show")
     display = "block"
   }
 
+  // This function is called when a user picks a language in the type ahead component:
+  //  (1) record the radio button checked in state
+  //  (2) if a language was selected, set it in state to this value and then select the correct radio button
+  //  (3) if no language was selected, set the language in state to null and then select the correct radio button
   const selectLanguage = (selected) => {
     setSubmitEnabled(true)
     if (selected.length === 1) {
       setLang(selected[0].id)
-      setRadioButtonValue("present")
+      setRadioButtonValue(langSelected)
     } else {
       setLang(null)
-      setRadioButtonValue("absent")
+      setRadioButtonValue(noLangSelected)
     }
   }
 
+  // This function is called when a user clicks one of the radio buttons:
+  //  (1) select the correct radio button
+  //  (2) if they selected the "no language" radio button, set language in state to null
   const handleLanguageRadio = (event) => {
-    if (event.target.value === "absent") setLang(null)
+    if (event.target.value === noLangSelected) setLang(null)
     setRadioButtonValue(event.target.value)
   }
 
@@ -55,7 +65,7 @@ const InputLang = (props) => {
   }
 
   const handleLangSubmit = (event) => {
-    if (radioButtonValue === "present" && lang === null) {
+    if (radioButtonValue === langSelected && lang === null) {
       setSubmitEnabled(false)
       return false
     }
@@ -75,15 +85,15 @@ const InputLang = (props) => {
               <input
                 type="radio"
                 className="form-check-input"
-                id="present"
-                value="present"
-                checked={radioButtonValue === "present"}
+                id={langSelected}
+                value={langSelected}
+                checked={radioButtonValue === langSelected}
                 onChange={handleLanguageRadio}
               />
-              <label className="form-check-label" htmlFor="present">
+              <label className="form-check-label" htmlFor={langSelected}>
                 Select language for {props.textValue}
                 <Typeahead
-                  disabled={radioButtonValue === "absent"}
+                  disabled={radioButtonValue === noLangSelected}
                   onChange={selectLanguage}
                   isLoading={props.loading}
                   options={props.options}
@@ -105,8 +115,8 @@ const InputLang = (props) => {
                 className="form-check-input"
                 id={`noLangRadio-${props.textValue}`}
                 data-testid={`noLangRadio-${props.textValue}`}
-                value="absent"
-                checked={radioButtonValue === "absent"}
+                value={noLangSelected}
+                checked={radioButtonValue === noLangSelected}
                 onChange={handleLanguageRadio}
               />
               <label
