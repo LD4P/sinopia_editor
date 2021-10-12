2b053b23762690a1c87277674f73c9db433494df
Naomi Dushay <ndushay@stanford.edu>
2021-10-12 12:27:43 -0700

    remove unused setAppVersion reducer and action

--- __tests__/reducers/index.test.js
+++ __tests__/reducers/index.test.js
@@ -1,25 +1,6 @@
 // Copyright 2020 Stanford University see LICENSE for license
 
-import { createReducer, setAppVersion } from "reducers/index"
-
-describe("setAppVersion()", () => {
-  it("sets the version of the application", () => {
-    const handlers = { SET_APP_VERSION: setAppVersion }
-
-    const reducer = createReducer(handlers)
-
-    // It would be impressive to get to version 2000.0.1
-    const action = { type: "SET_APP_VERSION", payload: "2000.0.1" }
-
-    const oldState = {
-      version: "3.0.0",
-    }
-
-    const newState = reducer(oldState, action)
-
-    expect(newState.version).toStrictEqual("2000.0.1")
-  })
-})
+import { createReducer } from "reducers/index"
 
 describe("createReducer", () => {
   it("handles the initial state", () => {
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,7 @@
 // Copyright 2019 Stanford University see LICENSE for license
-export const setAppVersion = (version) => ({
-  type: "SET_APP_VERSION",
-  payload: version,
-})
-
 export const setCurrentComponent = (rootSubjectKey, rootPropertyKey, key) => ({
   type: "SET_CURRENT_COMPONENT",
   payload: { rootSubjectKey, rootPropertyKey, key },
 })
+
+export const noop = () => {}
