479476c88dfac6db13a79460cf67ec3681787edd
Naomi Dushay <ndushay@stanford.edu>
2021-10-14 13:36:04 -0700

    move CopyToNewButton and PreviewButton next to resource title on right

--- src/components/editor/EditorActions.jsx
+++ src/components/editor/EditorActions.jsx
@@ -1,20 +1,17 @@
 // Copyright 2019 Stanford University see LICENSE for license
 
 import React from "react"
-import CopyToNewButton from "./actions/CopyToNewButton"
-import PreviewButton from "./actions/PreviewButton"
 import CloseButton from "./actions/CloseButton"
 import SaveAndPublishButton from "./actions/SaveAndPublishButton"
 import MarcButton from "./actions/MarcButton"
 import TransferButtons from "./actions/TransferButtons"
 
+// CopyToNewButton and PreviewButton are now called from ResourceComponent
 const EditorActions = () => (
   <div className="row">
     <div className="d-flex justify-content-end">
       <MarcButton />
       <TransferButtons />
-      <CopyToNewButton />
-      <PreviewButton />
       <CloseButton label={"Close"} />
       <SaveAndPublishButton class="editor-save" />
     </div>
