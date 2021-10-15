8a68dac93c148646abf126035b874f2b59d7ed9c
Peter Mangiafico <peter@mangiafico.org>
2021-10-15 14:52:33 -0700

    show full group names in preview header

--- src/components/editor/preview/ResourcePreviewHeader.jsx
+++ src/components/editor/preview/ResourcePreviewHeader.jsx
@@ -1,11 +1,17 @@
 // Copyright 2021 Stanford University see LICENSE for license
 
 import React, { useState } from "react"
+import { useSelector } from "react-redux"
 import PropTypes from "prop-types"
 import ResourceURIMessage from "../ResourceURIMessage"
+import { selectGroupMap } from "selectors/groups"
 
 const ResourcePreviewHeader = ({ resource }) => {
-  const editableBy = [...resource.editGroups, resource.group].join(", ") // the creator can also edit!
+  const groupMap = useSelector((state) => selectGroupMap(state))
+  const editableBy = [...resource.editGroups, resource.group] // the creator can also edit!
+    .map((group) => groupMap[group]) // look up the group name from the ID
+    .filter((group) => group) // ditch any undefined group (an unmatched groupID for whatever reason)
+    .join(", ")
   const maxGroupDisplay = 20
   const shouldTruncate = editableBy.length > maxGroupDisplay
   const [isCollapsed, setIsCollapsed] = useState(shouldTruncate)
@@ -29,12 +35,16 @@ const ResourcePreviewHeader = ({ resource }) => {
         </div>
         <div className="col-2">
           <strong>Owned by</strong>
-          <p>{resource.group}</p>
+          <p>{groupMap[resource.group] || "Unknown"}</p>
           <strong>Editable by</strong>
           <p>
             {editableByText}
             {isCollapsed && (
-              <button className="p-0 btn btn-link" onClick={handleClick}>
+              <button
+                data-testid="expand-groups-button"
+                className="p-0 btn btn-link"
+                onClick={handleClick}
+              >
                 ...
               </button>
             )}
