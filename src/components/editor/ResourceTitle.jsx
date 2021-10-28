b4b2c849c10645af6c0623185b8dfd6d7d51ee6f
Justin Coyne <jcoyne@justincoyne.com>
2021-10-28 16:19:47 -0500

    Prefix the title with the BF class
    Fixes #3126

---
+++ src/components/editor/ResourceTitle.jsx
@@ -0,0 +1,28 @@
+// Copyright 2019 Stanford University see LICENSE for license
+
+import React from "react"
+import PropTypes from "prop-types"
+import { isBfWork, isBfInstance, isBfItem } from "utilities/Bibframe"
+
+/**
+ * Shows the resources title
+ */
+const ResourceTitle = ({ resource }) => {
+  let badge = null
+  if (isBfWork(resource.classes)) badge = "WORK"
+  if (isBfInstance(resource.classes)) badge = "INSTANCE"
+  if (isBfItem(resource.classes)) badge = "ITEM"
+
+  return (
+    <React.Fragment>
+      {resource.label}
+      {badge && <span className="badge bg-secondary ms-2">{badge}</span>}
+    </React.Fragment>
+  )
+}
+
+ResourceTitle.propTypes = {
+  resource: PropTypes.object.isRequired,
+}
+
+export default ResourceTitle
