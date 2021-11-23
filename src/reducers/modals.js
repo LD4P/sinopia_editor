e608c9d7d19d26ab9a6d1238be08c1b3cf3aa8be
Justin Littman <justinlittman@stanford.edu>
2021-11-23 14:35:52 -0500

    Refactors modal state.

--- src/reducers/modals.js
+++ src/reducers/modals.js
@@ -1,27 +1,8 @@
-export const clearModalMessages = (state) => ({
-  ...state,
-  modal: {
-    ...state.modal,
-    messages: [],
-  },
-})
-
-export const addModalMessage = (state, action) => ({
-  ...state,
-  modal: {
-    ...state.modal,
-    messages: [...state.modal.messages, action.payload],
-  },
-})
-
 export const showModal = (state, action) => setModal(state, action.payload)
 
 export const hideModal = (state) => setModal(state, null)
 
 const setModal = (state, name) => ({
   ...state,
-  modal: {
-    ...state.modal,
-    name,
-  },
+  currentModal: name,
 })
